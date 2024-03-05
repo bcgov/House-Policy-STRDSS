ee583567c90a26ddc952a6819bbab80c724c80d1
ychung-mot <young-jin.chung@dxcas.com>
2024-03-05 08:08:58 -0800

    feat(dss-78 & dss-79): Keycloak integration

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,35 @@
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { KeycloakService } from 'keycloak-angular';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(routes),
+        KeycloakService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeKeycloak,
+            multi: true,
+            deps: [KeycloakService],
+        },
+    ],
+};
+
+function initializeKeycloak(keycloak: KeycloakService) {
+    return () =>
+        keycloak.init({
+            config: {
+                url: environment.SSO_HOST,
+                realm: environment.SSO_REALM,
+                clientId: environment.SSO_CLIENT,
+            },
+            initOptions: {
+                onLoad: 'login-required',
+                pkceMethod: 'S256',
+                // silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
+            },
+        });
+}
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ComponentsTestComponent } from './common/layout/components-test/components-test.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: ComponentsTestComponent
+    },
+];

5fd3bdb0de92751e52438fd332c65e8b44e1a10b
ychung-mot <young-jin.chung@dxcas.com>
2024-03-06 15:00:21 -0800

    feat(dss-91): a few endpoints for the story

---
+++ server/StrDss.Model/ComplianceNoticeDtos/ComplianceNoticeCreateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace StrDss.Model.ComplianceNoticeDtos
+{
+    public class ComplianceNoticeCreateDto
+    {
+        public long PlatformId { get; set; }
+        public string ListingUrl { get; set; } = "";
+        public string HostEmail { get; set; } = "";
+        public long ReasonId { get; set; }
+        public bool SendCopy { get; set; }
+        public List<string> CcList { get; set; } = new List<string>();
+        public string Comment { get; set; } = "";
+    }
+}

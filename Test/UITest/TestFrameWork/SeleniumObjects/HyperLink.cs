d2f04fb49739dcc94d1d966716afc4203f8d7837
Rick Anderson <Richand101@hotmail.com>
2024-03-06 10:31:21 -0800

    Adding Test Framework with Examples

---
+++ Test/UITest/TestFrameWork/SeleniumObjects/HyperLink.cs
@@ -0,0 +1,19 @@
+﻿using UITest.TestDriver;
+
+namespace UITest.SeleniumObjects
+{
+    public class HyperLink : UIElement
+    {
+        public HyperLink(IDriver Driver, Enums.FINDBY locatorType, string Locator) : base(Driver)
+        {
+            base.Locator = Locator;
+            base.LocatorType = LocatorType;
+        }
+
+        public bool Navigate()
+        {
+            return (base.Navigate(base.Locator));
+        }
+    }
+}
+

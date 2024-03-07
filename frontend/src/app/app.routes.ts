989d4faad02737299985b45a208f1efa42fe0a2f
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-03-07 13:50:19 -0600

    Removed old files. Added Compliance notice page.
    No backend integration yet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { ComponentsTestComponent } from './common/layout/components-test/components-test.component';
+import { ComplianceNoticeComponent } from './features/components/compliance-notice/compliance-notice.component';
+import { DashboardComponent } from './features/components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: ComponentsTestComponent
+        component: DashboardComponent,
     },
+    {
+        path: 'compliance-notice',
+        component: ComplianceNoticeComponent
+    }
 ];
---
+++ frontend/src/app/features/components/delisting-request/delisting-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delisting-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './delisting-request.component.html',
+  styleUrl: './delisting-request.component.scss'
+})
+export class DelistingRequestComponent {
+
+}

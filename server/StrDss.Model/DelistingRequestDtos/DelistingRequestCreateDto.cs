5875a6559e72249671deabec670a04960c99be80
ychung-mot <young-jin.chung@dxcas.com>
2024-03-11 10:22:41 -0700

    fix: delisting request with host email

--- server/StrDss.Model/DelistingRequestDtos/DelistingRequestCreateDto.cs
+++ server/StrDss.Model/DelistingRequestDtos/DelistingRequestCreateDto.cs
@@ -4,7 +4,6 @@
     {
         public long PlatformId { get; set; }
         public string ListingUrl { get; set; } = "";
-        public string HostEmail { get; set; } = "";
         public bool SendCopy { get; set; }
         public List<string> CcList { get; set; } = new List<string>();
         public string Comment { get; set; } = "";

93e4d1b38d60cd7be51e960dff2f4400fcd15654
Rick Anderson <Richand101@hotmail.com>
2024-03-12 15:24:18 -0700

    Added STR pages and models

---
+++ Test/UITest/TestFrameWork/Models/DelistingRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium.DevTools.V118.DOM;
+
+namespace UITest.Models
+{
+    public class DelistingRequestModel
+    {
+        public static string RequestInitiatedByDropDown { get => ""; }
+        public static string PlaformRecepientDropDown { get => "platformId"; }
+        public static string ListingIDNumberTextBox { get => ""; }
+        public static string ListingUrlTextBox { get => "listingUrl"; }
+        public static string SendCopyCheckbox { get => "sendCopy"; }
+        public static string AdditionalCCsTextBox { get => "ccList"; }
+        public static string ReviewButton { get => "form-preview-btn"; }
+    }
+}

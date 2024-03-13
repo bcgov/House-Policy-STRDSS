5bf52820e1cc2e8c896f1a247f10f8bc45b9aedb
ychung-mot <young-jin.chung@dxcas.com>
2024-03-13 10:12:51 -0700

    feat(dss-91): delisting warning

--- server/StrDss.Test/AutoDomainDataAttribute.cs
+++ server/StrDss.Test/AutoDomainDataAttribute.cs
@@ -1,52 +1,79 @@
-﻿using AutoFixture;
+﻿using System.Reflection;
+using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Kernel;
 using AutoFixture.Xunit2;
-using System.Reflection;
+using StrDss.Model.DelistingDtos;
 
 namespace StrDss.Test
 {
+    public class DelistingWarningCreateDtoSpecimenBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as PropertyInfo;
+
+            if (pi == null)
+                return new NoSpecimen();
+
+            switch (pi.Name)
+            {
+                case nameof(DelistingWarningCreateDto.PlatformId):
+                    return 1;
+                case nameof(DelistingWarningCreateDto.ListingId):
+                    return 1;
+                case nameof(DelistingWarningCreateDto.ReasonId):
+                    return 1;
+
+                case nameof(DelistingWarningCreateDto.ListingUrl):
+                    return "https://example.com/listing";
+
+                case nameof(DelistingWarningCreateDto.HostEmail):
+                    return "host@example.com";
+
+                case nameof(DelistingWarningCreateDto.HostEmailSent):
+                    return false;
+
+                case nameof(DelistingWarningCreateDto.SendCopy):
+                    return true;
+
+                case nameof(DelistingWarningCreateDto.CcList):
+                    return new List<string> { "cc1@example.com", "cc2@example.com" };
+
+                case nameof(DelistingWarningCreateDto.LgContactEmail):
+                    return "lg@example.com";
+
+                case nameof(DelistingWarningCreateDto.LgContactPhone):
+                    return "1234567890";
+
+                case nameof(DelistingWarningCreateDto.StrBylawUrl):
+                    return "https://example.com/bylaw";
+
+                case nameof(DelistingWarningCreateDto.Comment):
+                    return "This is a comment.";
+
+                default:
+                    return new NoSpecimen();
+            }
+        }
+    }
+
     [AttributeUsage(AttributeTargets.Method)]
     public class AutoDomainDataAttribute : AutoDataAttribute
     {
         public AutoDomainDataAttribute()
             : base(() => new Fixture()
                 .Customize(new AutoMoqCustomization())
-                .Customize(new EmailCustomization()))
+                .Customize(new DelistingWarningCreateDtoCustomization()))
         {
         }
     }
 
-    public class EmailCustomization : ICustomization
+    public class DelistingWarningCreateDtoCustomization : ICustomization
     {
         public void Customize(IFixture fixture)
         {
-            fixture.Customizations.Add(new SpecimenBuilder());
-        }
-    }
-
-    public class SpecimenBuilder : ISpecimenBuilder
-    {
-        public object Create(object request, ISpecimenContext context)
-        {
-            var pi = request as PropertyInfo;
-
-            if (pi == null || pi.PropertyType != typeof(string))
-                return new NoSpecimen();
-
-            switch (pi.Name.ToLower())
-            {
-                case "email":
-                    return "user@user.com";
-                case "streetaddress":
-                    return "940 Blanshard St";
-                case "city":
-                    return "Victoria";
-                case "province":
-                    return "BC";
-            }
-
-            return new NoSpecimen();
+            fixture.Customizations.Add(new DelistingWarningCreateDtoSpecimenBuilder());
         }
     }
 }

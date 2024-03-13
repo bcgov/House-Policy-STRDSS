01e84dc5cfdf47d5fe3fe30d53febb7527b896d3
ychung-mot <young-jin.chung@dxcas.com>
2024-03-13 10:56:21 -0700

    chore: delisting request

---
+++ server/StrDss.Model/LocalGovernmentDtos/LocalGovernmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace StrDss.Model.LocalGovernmentDtos
+{
+    public class LocalGovernmentDto
+    {
+        public long LocalGovernmentId { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public static readonly LocalGovernmentDto[] localGovernments = new LocalGovernmentDto[]
+        {
+            new LocalGovernmentDto {LocalGovernmentId = 1, Name = "Victoria", Email = "young-jin.chung@gov.bc.ca"},
+            new LocalGovernmentDto {LocalGovernmentId = 2, Name = "Vancouver", Email = "young-jin.chung@gov.bc.ca" }
+        };
+    }
+}

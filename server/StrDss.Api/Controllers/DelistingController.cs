99d6a3fe7a292d8e622ba6d49ab3abd6e97a64de
ychung-mot <young-jin.chung@dxcas.com>
2024-03-13 10:56:21 -0700

    chore: delisting request

--- server/StrDss.Api/Controllers/DelistingController.cs
+++ server/StrDss.Api/Controllers/DelistingController.cs
@@ -5,6 +5,7 @@ using StrDss.Api.Authorization;
 using StrDss.Common;
 using StrDss.Model;
 using StrDss.Model.DelistingDtos;
+using StrDss.Model.LocalGovernmentDtos;
 using StrDss.Model.PlatformDtos;
 using StrDss.Service;
 using StrDss.Service.HttpClients;
@@ -32,7 +33,6 @@ namespace StrDss.Api.Controllers
             _delistingService = delistingService;
         }
 
-        #region Warning
         [HttpGet("reasons/dropdown", Name = "GetWarningReasonDrowdown")]
         [ApiAuthorize]
         public async Task<ActionResult<DropdownDto>> GetWarningReasonDrowdown()
@@ -86,22 +86,20 @@ namespace StrDss.Api.Controllers
             return new EmailPreview { Content = _delistingService.FormatDelistingWarningEmailContent(dto, toList, false) };
         }
 
-        #endregion
-
-        #region Request
         [HttpPost("requests", Name = "CreateDelistingRequest")]
         [ApiAuthorize]
         public async Task<ActionResult> CreateDelistingRequest(DelistingRequestCreateDto dto)
         {
             var platform = PlatformDto.Platforms.FirstOrDefault(x => x.PlatformId == dto.PlatformId);
+            var lg = LocalGovernmentDto.localGovernments.FirstOrDefault(x => x.LocalGovernmentId == dto.LgId);
 
-            var validationResult = ValidateDelistingRequest(dto, platform);
-            if (!validationResult.IsValid)
+            var errors = await _delistingService.ValidateDelistingRequest(dto, platform, lg);
+            if (errors.Count > 0)
             {
-                return validationResult.Result;
+                return ValidationUtils.GetValidationErrorResult(errors, ControllerContext);
             }
 
-            var error = await SendDelistingRequestAsync(dto, platform);
+            var error = await _delistingService.SendDelistingRequestAsync(dto, platform);
 
             if (!error.IsEmpty())
             {
@@ -111,116 +109,22 @@ namespace StrDss.Api.Controllers
             return NoContent();
         }
 
-        private async Task<string> SendDelistingRequestAsync(DelistingRequestCreateDto dto, PlatformDto? platform)
-        {
-            try
-            {
-                var token = await _chesTokenApi.GetTokenAsync();
-                var chesUrl = _config.GetValue<string>("CHES_URL") ?? "";
-
-                using HttpClient httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
-
-                var emailContent = new
-                {
-                    bcc = new string[] { },
-                    bodyType = "text",
-                    body = FormatDelistingRequestEmailContent(dto, true),
-                    cc = dto.CcList.ToArray(),
-                    delayTS = 0,
-                    encoding = "utf-8",
-                    from = "no_reply@gov.bc.ca",
-                    priority = "normal",
-                    subject = "Compliance Notice",
-                    to = new string[] { platform?.Email ?? "" },
-                };
-
-                var jsonContent = Newtonsoft.Json.JsonConvert.SerializeObject(emailContent);
-                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-                var response = await httpClient.PostAsync($"{chesUrl}/api/v1/email", httpContent);
-                if (response.IsSuccessStatusCode)
-                {
-                    _logger.LogInformation($"Sent delisting request to {emailContent.to} for {dto.ListingUrl}");
-                }
-                else
-                {
-                    _logger.LogError($"Failed to send email. Status code: {response.StatusCode}");
-                }
-
-            }
-            catch (Exception ex)
-            {
-                var error = $"Sending email failed with an exception";
-                _logger.LogError($"{error} - {ex}");
-                return error;
-            }
-
-            _logger.LogInformation($"Sent compliance notice.");
-            return "";
-        }
-
         [HttpPost("requests/preview", Name = "GetDelistingRequestPreview")]
         [ApiAuthorize]
         public async Task<ActionResult<EmailPreview>> GetDelistingRequestPreview(DelistingRequestCreateDto dto)
         {
-            await Task.CompletedTask;
-
             var platform = PlatformDto.Platforms.FirstOrDefault(x => x.PlatformId == dto.PlatformId);
+            var lg = LocalGovernmentDto.localGovernments.FirstOrDefault(x => x.LocalGovernmentId == dto.LgId);
 
-            var validationResult = ValidateDelistingRequest(dto, platform);
-            if (!validationResult.IsValid)
-            {
-                return validationResult.Result;
-            }
-
-            return new EmailPreview { Content = FormatDelistingRequestEmailContent(dto, false) };
-        }
-
-        private (bool IsValid, ActionResult Result) ValidateDelistingRequest(DelistingRequestCreateDto dto, PlatformDto? platform)
-        {
-            var errors = new Dictionary<string, List<string>>();
-
-            if (platform == null)
-            {
-                errors.AddItem("platformId", $"Platform ID ({dto.PlatformId}) does not exist.");
-            }
-
-            var regex = RegexDefs.GetRegexInfo(RegexDefs.Email);
-
-            foreach (var email in dto.CcList)
-            {
-                if (!Regex.IsMatch(email, regex.Regex))
-                {
-                    errors.AddItem("ccList", $"Email ({email}) is invalid");
-                }
-            }
-
+            var errors = await _delistingService.ValidateDelistingRequest(dto, platform, lg);
             if (errors.Count > 0)
             {
-                return (false, ValidationUtils.GetValidationErrorResult(errors, ControllerContext));
+                return ValidationUtils.GetValidationErrorResult(errors, ControllerContext);
             }
 
-            if (dto.SendCopy)
-            {
-                dto.CcList.Add(_currentUser.EmailAddress);
-            }
+            var toList = new List<string> { platform?.Email ?? "" };
 
-            return (true, null);
+            return new EmailPreview { Content = _delistingService.FormatDelistingRequestEmailContent(dto, toList, false) };
         }
-
-        private string FormatDelistingRequestEmailContent(DelistingRequestCreateDto dto, bool contentOnly)
-        {
-            var platform = PlatformDto.Platforms.FirstOrDefault(x => x.PlatformId == dto.PlatformId);
-
-            return (contentOnly ? "" : $@"To: {platform?.Name}{Environment.NewLine}cc: {string.Join(";", dto.CcList)}{Environment.NewLine}{Environment.NewLine}")
-                 + $@"Dear Sir/Madam,{Environment.NewLine}"
-                 + $@"{Environment.NewLine}The following listing has been found non-compliance to current STR regulations:{Environment.NewLine}"
-                 + $@"{dto.ListingUrl}{Environment.NewLine}"
-                 + $@"{Environment.NewLine}Please remove this listing from your platform immediately to mitigate the need for any further actions.{Environment.NewLine}"
-                 + (dto.Comment.IsEmpty() ? "" : $@"{Environment.NewLine}{dto.Comment}{Environment.NewLine}")
-                 + $@"{Environment.NewLine}Kind Regards,{Environment.NewLine}";
-        }
-        #endregion
     }
 }

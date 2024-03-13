e55be9b8e0ac8d18a425d110b7abda614858b8d4
ychung-mot <young-jin.chung@dxcas.com>
2024-03-13 07:37:04 -0700

    chore: email preview

--- server/StrDss.Model/DelistingDtos/DelistingWarningCreateDto.cs
+++ server/StrDss.Model/DelistingDtos/DelistingWarningCreateDto.cs
@@ -3,8 +3,10 @@
     public class DelistingWarningCreateDto
     {
         public long PlatformId { get; set; }
+        public long ListingId { get; set; }
         public string ListingUrl { get; set; } = "";
         public string HostEmail { get; set; } = "";
+        public bool HostEmailSent { get; set; }
         public long ReasonId { get; set; }
         public bool SendCopy { get; set; }
         public List<string> CcList { get; set; } = new List<string>();

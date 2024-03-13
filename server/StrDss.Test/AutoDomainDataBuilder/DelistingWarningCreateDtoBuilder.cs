5d46adcf23d6dd5b89b78044464ff96c242c119d
ychung-mot <young-jin.chung@dxcas.com>
2024-03-13 15:07:25 -0700

    chore: refactoring

---
+++ server/StrDss.Test/AutoDomainDataBuilder/DelistingWarningCreateDtoBuilder.cs
@@ -0,0 +1,66 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using StrDss.Model.DelistingDtos;
+using System.Reflection;
+
+namespace StrDss.Test.AutoDomainDataBuilder
+{
+    public class DelistingWarningCreateDtoCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customizations.Add(new DelistingWarningCreateDtoBuilder());
+        }
+    }
+    public class DelistingWarningCreateDtoBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as PropertyInfo;
+
+            if (pi == null)
+                return new NoSpecimen();
+
+            switch (pi.Name)
+            {
+                case nameof(DelistingWarningCreateDto.PlatformId):
+                    return 1;
+                case nameof(DelistingWarningCreateDto.ListingId):
+                    return 1;
+                case nameof(DelistingWarningCreateDto.ReasonId):
+                    return 1;
+
+                case nameof(DelistingWarningCreateDto.ListingUrl):
+                    return "https://example.com/listing";
+
+                case nameof(DelistingWarningCreateDto.HostEmail):
+                    return "host@example.com";
+
+                case nameof(DelistingWarningCreateDto.HostEmailSent):
+                    return false;
+
+                case nameof(DelistingWarningCreateDto.SendCopy):
+                    return true;
+
+                case nameof(DelistingWarningCreateDto.CcList):
+                    return new List<string> { "cc1@example.com", "cc2@example.com" };
+
+                case nameof(DelistingWarningCreateDto.LgContactEmail):
+                    return "lg@example.com";
+
+                case nameof(DelistingWarningCreateDto.LgContactPhone):
+                    return "1234567890";
+
+                case nameof(DelistingWarningCreateDto.StrBylawUrl):
+                    return "https://example.com/bylaw";
+
+                case nameof(DelistingWarningCreateDto.Comment):
+                    return "This is a comment.";
+
+                default:
+                    return new NoSpecimen();
+            }
+        }
+    }
+
+}

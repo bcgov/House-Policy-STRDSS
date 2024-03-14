0cadf8a12007c19b30fb0c3a5e148fb224277ba9
Rick Anderson <Richand101@hotmail.com>
2024-03-13 21:26:28 -0700

    Added AppSettings class for app setings mgmt, Request delist test updates

---
+++ Test/UITest/Configuration/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    public class AppSettings
+    {
+        IConfiguration _Configuration;
+        string environment;
+
+        public AppSettings()
+        {
+            environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Local";
+            _Configuration = new ConfigurationBuilder().AddJsonFile($"Appsettings.{environment}.json", optional: true, reloadOnChange: true).Build();
+        }
+
+        public string GetValue(string Variable)
+        {
+            string variableValue = _Configuration[Variable];
+
+            if (variableValue != null)
+            {
+                return (variableValue);
+            }
+            else
+            {
+                return (null);
+            }
+        }
+
+    }
+
+}
+

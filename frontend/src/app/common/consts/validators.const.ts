7fb9cb3fd5879d9e5fbc6789f231cbb3c2b9c0f2
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-03-14 22:09:19 -0500

    Notice of takedown

--- frontend/src/app/common/consts/validators.const.ts
+++ frontend/src/app/common/consts/validators.const.ts
@@ -15,16 +15,40 @@ export function validateUrl(): ValidatorFn {
     };
 }
 
-export function validateEmailChips(): ValidatorFn {
+export function validatePhone(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-        const emailList = control.value as Array<string>;
+        const url = control.value;
 
-        if (!emailList) {
+        if (!url) {
+            return null;
+        }
+
+        const phoneRegex = new RegExp(/^\(\d{3}\) \d{3}-\d{4}$/);
+        const validUrl = phoneRegex.test(url);
+
+        return validUrl ? null : { invalidPhone: true };
+    };
+}
+
+export function validateEmailListString(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        let emailList;
+
+        if (control.value.constructor === Array) {
+            emailList = control.value;
+        }
+        else {
+            emailList = (control.value as string).split(',').filter(x => !!x).map(x => x.trim()) as Array<string>;
+        }
+
+        if (!emailList || !emailList.length) {
             return null;
         }
 
         const emailRegex = new RegExp(/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/);
-        const allValid = emailList.every((email) => emailRegex.test(email))
+        const allValid = emailList.every((email: string) => {
+            return emailRegex.test(email)
+        })
 
         return allValid ? null : { invalidEmailChips: true };
     };
--- frontend/src/app/common/models/compliance-notice.ts
+++ frontend/src/app/common/models/compliance-notice.ts
@@ -1,9 +1,14 @@
 export interface ComplianceNotice {
     platformId: number;
+    listingId?: string;
     listingUrl: string;
-    hostEmail: string;
+    hostEmail?: string;
+    sentAlternatively: boolean;
     reasonId: number;
     sendCopy: boolean;
     ccList: Array<string>;
+    LgContactEmail: string;
+    LgContactPhone?: string;
+    StrBylawUrl?: string;
     comment: string;
 }

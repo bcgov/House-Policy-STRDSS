d4d0aeca0d01eda22bcd8ae3db44f562f97a6849
ychung-mot <young-jin.chung@dxcas.com>
2024-03-18 07:27:19 -0700

    chore: database scaffolding

---
+++ server/StrDss.Data/Entities/DssUserPrivilege.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrDss.Data.Entities;
+
+/// <summary>
+/// A granular access right or privilege within the application that may be granted to a role
+/// </summary>
+public partial class DssUserPrivilege
+{
+    /// <summary>
+    /// Unique generated key
+    /// </summary>
+    public long UserPrivilegeId { get; set; }
+
+    /// <summary>
+    /// The immutable system code that identifies the privilege
+    /// </summary>
+    public string PrivilegeCd { get; set; } = null!;
+
+    /// <summary>
+    /// The human-readable name that is given for the role
+    /// </summary>
+    public string PrivilegeNm { get; set; } = null!;
+
+    public virtual ICollection<DssUserRolePrivilege> DssUserRolePrivileges { get; set; } = new List<DssUserRolePrivilege>();
+}

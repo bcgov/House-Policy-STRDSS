945d8d64e84621a04aea3eadd1baaff28e95b83d
ychung-mot <young-jin.chung@dxcas.com>
2024-03-18 15:39:51 -0700

    feat(dss-139, 149): Access Request Submission

---
+++ server/StrDss.Model/UserDtos/AccessRequestDto.cs
@@ -0,0 +1,33 @@
+﻿namespace StrDss.Model.UserDtos
+{
+    public class AccessRequestDto
+    {
+        public long UserIdentityId { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public string AccessRequestStatusDsc { get; set; } = null!;
+
+        public DateTime? AccessRequestDtm { get; set; }
+
+        public string? AccessRequestJustificationTxt { get; set; }
+
+        public string? GivenNm { get; set; }
+
+        public string? FamilyNm { get; set; }
+
+        public string? EmailAddressDsc { get; set; }
+
+        public string? BusinessNm { get; set; }
+
+        public DateTime? TermsAcceptanceDtm { get; set; }
+
+        public long? RepresentedByOrganizationId { get; set; }
+
+        public string OrganizationType { get; set; } = null!;
+
+        public string OrganizationCd { get; set; } = null!;
+
+        public string OrganizationNm { get; set; } = null!;
+    }
+}

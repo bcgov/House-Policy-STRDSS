01d8418505a8d667bd1012780319a2e28385792c
ychung-mot <young-jin.chung@dxcas.com>
2024-03-21 07:50:25 -0700

    feat(dss-147): access request deny

---
+++ server/StrDss.Model/UserDtos/AccessRequestDenyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StrDss.Model.UserDtos
+{
+    public class AccessRequestDenyDto
+    {
+        public long UserIdentityId { get; set; }
+        public DateTime UpdDtm { get; set; }
+    }
+}

6d71a53b3ea9eff43ede0c9518cbc841fc780df1
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-03-21 11:56:42 -0500

    Access Request. Validation fix. New API endpoinds fix.

---
+++ frontend/src/app/features/components/access-request/access-request.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccessRequestComponent } from './access-request.component';
+
+describe('AccessRequestComponent', () => {
+  let component: AccessRequestComponent;
+  let fixture: ComponentFixture<AccessRequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AccessRequestComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AccessRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

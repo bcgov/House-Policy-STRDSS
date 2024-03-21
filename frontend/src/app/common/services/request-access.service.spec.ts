5e880ad65fdc2a203ce7aefeec3979fb059ce687
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-03-21 17:52:11 -0500

    [Feature]: Access Request. [Fix]: platformId type. Styles. unit tests

--- frontend/src/app/common/services/request-access.service.spec.ts
+++ frontend/src/app/common/services/request-access.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RequestAccessService } from './request-access.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('RequestAccessService', () => {
   let service: RequestAccessService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(RequestAccessService);
   });
 

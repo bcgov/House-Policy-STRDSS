a4e273ec2bd54bf1c54ac4cea2e713392a306637
ychung-mot <young-jin.chung@dxcas.com>
2024-03-21 15:13:55 -0700

    fix: org dropdown

---
+++ server/StrDss.Model/DropdownNumDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StrDss.Model
+{
+    public class DropdownNumDto
+    {
+        [JsonPropertyName("value")]
+        public long Id { get; set; }
+        [JsonPropertyName("label")]
+        public string Description { get; set; } = "";
+    }
+}

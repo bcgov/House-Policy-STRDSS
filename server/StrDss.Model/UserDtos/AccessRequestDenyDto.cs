78453910d07d2cfff7ce4e9c26204ddd542a4bf4
ychung-mot <young-jin.chung@dxcas.com>
2024-03-21 09:05:40 -0700

    chore: approve access request

--- server/StrDss.Model/UserDtos/AccessRequestDenyDto.cs
+++ server/StrDss.Model/UserDtos/AccessRequestDenyDto.cs
@@ -1,8 +1,13 @@
-﻿namespace StrDss.Model.UserDtos
+﻿using StrDss.Common;
+using System.Text.Json.Serialization;
+
+namespace StrDss.Model.UserDtos
 {
     public class AccessRequestDenyDto
     {
         public long UserIdentityId { get; set; }
+        [JsonIgnore]
+        public string AccessRequestStatusCd { get; set; } = AccessRequestStatuses.Denied;
         public DateTime UpdDtm { get; set; }
     }
 }

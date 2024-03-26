95bb6920de876f94f89363f14f840880748603b4
ychung-mot <young-jin.chung@dxcas.com>
2024-03-26 12:07:05 -0700

    chore: access request statuses & idp

--- server/StrDss.Model/WarningReasonDtos/WarningReasonDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StrDss.Model.WarningReasonDtos
-{
-    public class WarningReasonDto
-    {
-        public long WarningReasonId { get; set; }
-        public string Reason { get; set; } = "";
-    }
-}

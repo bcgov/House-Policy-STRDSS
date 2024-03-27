211e74e2ea727a47a0bd7b6f8e1da6d8e69f9c4c
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-03-27 18:02:55 -0500

    UserList changes: 1.Modified date format for a table. 2.Updated columns by design. 3.Added and integrated isActive switch. 4.Added confirmation popup for activation/deactivation. 5.Renamed component and related files from AccessRequestList to UserManagement. 6.Fixed some styles.

---
+++ frontend/src/app/common/pipes/date-format.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateFormatPipe } from './date-format.pipe';
+
+xdescribe('DateFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});

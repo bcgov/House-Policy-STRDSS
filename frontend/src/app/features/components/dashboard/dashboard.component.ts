3d4749aecd68d5d2a7f41639707795e469bf0f50
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-04-01 18:00:20 -0500

    A basic set of components for DSS-137

--- frontend/src/app/features/components/dashboard/dashboard.component.ts
+++ frontend/src/app/features/components/dashboard/dashboard.component.ts
@@ -2,24 +2,17 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { LgStaffUserScreenComponent } from './lg-staff-user-screen/lg-staff-user-screen.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, CardModule, ButtonModule],
+  imports: [RouterOutlet, CardModule, ButtonModule, LgStaffUserScreenComponent,],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   constructor(private router: Router) {
 
-  }
-
-  toComplianceNotice(): void {
-    this.router.navigateByUrl('/compliance-notice')
-  }
-
-  toDelistingRequest(): void {
-    this.router.navigateByUrl('/delisting-request')
   }
 }

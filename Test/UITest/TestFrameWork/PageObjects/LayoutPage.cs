6ef04b93116251c84092cb49eb31134e799fb450
Rick Anderson <Richand101@hotmail.com>
2024-04-04 16:07:06 -0700

    Created PageObjects and Models for Managing Access

--- Test/UITest/TestFrameWork/PageObjects/LayoutPage.cs
+++ Test/UITest/TestFrameWork/PageObjects/LayoutPage.cs
@@ -4,7 +4,7 @@ using UITest.TestDriver;
 
 namespace UITest.PageObjects
 {
-    public class DashboardPage
+    public class LayoutPage
     {
         private TextBox _AddApplicationButton;
         private string _URL = @"localhost:5002/login";
@@ -15,11 +15,11 @@ namespace UITest.PageObjects
         public string URL { get => _URL; set => _URL = value; }
         public Button LogoutButton { get => _LogoutButton;  }
 
-        public DashboardPage(IDriver Driver)
+        public LayoutPage(IDriver Driver)
         {
             _Driver = Driver;
-            _AddApplicationButton = new TextBox(Driver, Enums.FINDBY.CSSSELECTOR, DashboardPageModel.AddApplicationButton);
-            _LogoutButton = new Button(Driver, Enums.FINDBY.CSSSELECTOR, DashboardPageModel.LogoutButton);
+            _AddApplicationButton = new TextBox(Driver, Enums.FINDBY.CSSSELECTOR, LayoutPageModel.AddApplicationButton);
+            _LogoutButton = new Button(Driver, Enums.FINDBY.CSSSELECTOR, LayoutPageModel.LogoutButton);
         }
 
         public bool Navigate()

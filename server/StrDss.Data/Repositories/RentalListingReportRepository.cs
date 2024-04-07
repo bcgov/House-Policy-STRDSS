3bb5c4930da60c22ae26436e51b6c01160cb045d
ychung-mot <young-jin.chung@dxcas.com>
2024-04-06 19:57:55 -0700

    chore: scaffolding & save to rental listing report

---
+++ server/StrDss.Data/Repositories/RentalListingReportRepository.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using StrDss.Data.Entities;
+using StrDss.Model;
+
+namespace StrDss.Data.Repositories
+{
+    public interface IRentalListingReportRepository
+    {
+        Task AddRentalLisitngReportAsync(DssRentalListingReport report);
+    }
+    public class RentalListingReportRepository : RepositoryBase<DssRentalListingReport>, IRentalListingReportRepository
+    {
+        public RentalListingReportRepository(DssDbContext dbContext, IMapper mapper, ICurrentUser currentUser) 
+            : base(dbContext, mapper, currentUser)
+        {
+        }
+
+        public async Task AddRentalLisitngReportAsync(DssRentalListingReport report)
+        {
+            await _dbSet.AddAsync(report);
+        }
+    }
+}

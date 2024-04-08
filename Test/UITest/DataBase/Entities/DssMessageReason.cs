5ca356c7e85e807bbd6ff997b69af0cbf0c54e87
Rick Anderson <Richand101@hotmail.com>
2024-04-08 16:10:48 -0700

    Created  DB Project which will used for Creating test data

---
+++ Test/UITest/DataBase/Entities/DssMessageReason.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBase.Entities;
+
+public partial class DssMessageReason
+{
+    public long MessageReasonId { get; set; }
+
+    public string EmailMessageType { get; set; } = null!;
+
+    /// <summary>
+    /// A description of the justification for initiating a message
+    /// </summary>
+    public string MessageReasonDsc { get; set; } = null!;
+
+    public virtual ICollection<DssEmailMessage> DssEmailMessages { get; } = new List<DssEmailMessage>();
+
+    public virtual DssEmailMessageType EmailMessageTypeNavigation { get; set; } = null!;
+}
---
+++ Test/UITest/DataBase/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using DataBase.Entities;
+using DataBase.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<DssAccessRequestStatus> DssAccessRequestStatusRepository { get; }
+        GenericRepository<DssEmailMessage> DssEmailMessageRepository { get; }
+        GenericRepository<DssEmailMessageType> DssEmailMessageTypeRepository { get; }
+        GenericRepository<DssMessageReason> DssMessageReasonRepository { get; }
+        GenericRepository<DssOrganization> DssOrganizationRepository { get; }
+        GenericRepository<DssOrganizationContactPerson> DssOrganizationContactPersonRepository { get; }
+        GenericRepository<DssOrganizationType> DssOrganizationTypeRepository { get; }
+        GenericRepository<DssUserIdentity> DssUserIdentityRepository { get; }
+        GenericRepository<DssUserIdentityView> DssUserIdentityViewRepository { get; }
+        GenericRepository<DssUserPrivilege> DssUserPrivilegeRepository { get; }
+        GenericRepository<DssUserRole> DssUserRoleRepository { get; }
+
+        void Dispose();
+        void Save();
+    }
+}

2e8bbb68016c8b4ef24e82b8ccfbe975fd7c2f02
Rick Anderson <Richand101@hotmail.com>
2024-04-08 16:10:48 -0700

    Created  DB Project which will used for Creating test data

---
+++ Test/UITest/DataBase/Entities/DssEmailMessage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBase.Entities;
+
+/// <summary>
+/// A message that is sent to one or more recipients via email
+/// </summary>
+public partial class DssEmailMessage
+{
+    /// <summary>
+    /// Unique generated key
+    /// </summary>
+    public long EmailMessageId { get; set; }
+
+    /// <summary>
+    /// Business term for the type or purpose of the message (e.g. Notice of Takedown, Takedown Request, Delisting Warning, Delisting Request, Access Granted Notification, Access Denied Notification)
+    /// </summary>
+    public string EmailMessageType { get; set; } = null!;
+
+    /// <summary>
+    /// A timestamp indicating when the message delivery was initiated
+    /// </summary>
+    public DateTime MessageDeliveryDtm { get; set; }
+
+    /// <summary>
+    /// The full text or template for the message that is sent
+    /// </summary>
+    public string MessageTemplateDsc { get; set; } = null!;
+
+    public bool IsHostContactedExternally { get; set; }
+
+    public bool IsSubmitterCcRequired { get; set; }
+
+    public long? MessageReasonId { get; set; }
+
+    public string? LgPhoneNo { get; set; }
+
+    public string? UnreportedListingNo { get; set; }
+
+    /// <summary>
+    /// E-mail address of a short term rental host (directly entered by the user as a message recipient)
+    /// </summary>
+    public string? HostEmailAddressDsc { get; set; }
+
+    public string? LgEmailAddressDsc { get; set; }
+
+    /// <summary>
+    /// E-mail address of a secondary message recipient (directly entered by the user)
+    /// </summary>
+    public string? CcEmailAddressDsc { get; set; }
+
+    /// <summary>
+    /// User-provided URL for a short-term rental platform listing that is the subject of the message
+    /// </summary>
+    public string? UnreportedListingUrl { get; set; }
+
+    public string? LgStrBylawUrl { get; set; }
+
+    /// <summary>
+    /// Foreign key
+    /// </summary>
+    public long InitiatingUserIdentityId { get; set; }
+
+    /// <summary>
+    /// Foreign key
+    /// </summary>
+    public long? AffectedByUserIdentityId { get; set; }
+
+    /// <summary>
+    /// Foreign key
+    /// </summary>
+    public long? InvolvedInOrganizationId { get; set; }
+
+    public virtual DssUserIdentity? AffectedByUserIdentity { get; set; }
+
+    public virtual DssEmailMessageType EmailMessageTypeNavigation { get; set; } = null!;
+
+    public virtual DssUserIdentity InitiatingUserIdentity { get; set; } = null!;
+
+    public virtual DssOrganization? InvolvedInOrganization { get; set; }
+
+    public virtual DssMessageReason? MessageReason { get; set; }
+}
---
+++ Test/UITest/DataBase/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity GetByID(object id);
+        TEntity GetOrInsert(TEntity entity, bool updateReferenceData, Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}

5cd6d8f2b741310dd161807a2c19d0a27d2c468a
ychung-mot <young-jin.chung@dxcas.com>
2024-04-09 08:49:30 -0700

    chore: bceid soap

---
+++ server/StrDss.Service/Bceid/BceidAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace StrDss.Service.Bceid
+{
+    public class BceidAccount
+    {
+        public string Username { get; set; } = null!;
+        public Guid UserGuid { get; set; }
+        public Guid? BusinessGuid { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string BusinessLegalName { get; set; } = null!;
+        public decimal BusinessNumber { get; set; } 
+        public string DoingBusinessAs { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string UserType { get; set; } = null!;
+        public string DisplayName { get; set; } = null!;
+    }
+}

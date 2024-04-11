ae63ec073eff5ea3c67d1e217937adddd263cd38
ychung-mot <young-jin.chung@dxcas.com>
2024-04-11 08:56:06 -0700

    feat(dss-262): batch takedown request

--- server/StrDss.Service/EmailTemplates/TakedownRequest.cs
+++ server/StrDss.Service/EmailTemplates/TakedownRequest.cs
@@ -11,21 +11,17 @@ namespace StrDss.Service.EmailTemplates
 
         public string Url { get; set; } = "";
         public string? ListingId { get; set; }
-        public string LgContactInfo { get; set; } = "";
-        public string LgName { get; set; } = "";
         public override string GetContent()
         {
-            Subject = "Takedown Request";
+            Subject = "Confirmation of Takedown Request";
 
             return (Preview ? GetPreviewContent() : "") + $@"
-<b>Request to platform service provider for takedown of non-compliant platform offering.</b><br/><br/>
-The following short-term rental listing is not in compliance with an applicable local government business licence requirement:<br/><br/>
+<b>A takedown request for the following short-term rental listing was submitted to the STR Data Portal and will be delivered to the respective short-term rental platform at 11:50pm PST tonight:</b><br/><br/>
 <b>{Url}</b><br/><br/>
 Listing ID Number: <b>{ListingId}</b><br/><br/>
-In accordance, with 17(2) of the Short-term Rental Accommodations Act, please cease providing platform services in respect of the above platform offer within 3 days.<br/><br/>
-<b>{LgName}</b><br/><br/>
-<b>{LgContactInfo}</b>
+The platform is required to remove the listing within 8 days of the date it was delivered. If the platform fails to remove the listing, local governments can escalate the matter to the Director of the Provincial STR Compliance and Enforcement Unit at: CEUescalations@gov.bc.ca.<br/><br/>
+This email has been automatically generated. Please do not reply to this email.
 ";
         }
-    }
+    }   
 }

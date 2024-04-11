3ff3729a61b9b39af417f05fa2f1f97a075a6125
ychung-mot <young-jin.chung@dxcas.com>
2024-04-11 08:56:06 -0700

    feat(dss-262): batch takedown request

---
+++ server/StrDss.Model/TakedownRequestCsvRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace StrDss.Model
+{
+    public class TakedownRequestCsvRecord
+    {
+        public string Url { get; set; } = "";
+        public string ListingId { get; set; } = "";
+        public string RequestedBy { get; set; } = "";
+    }
+}

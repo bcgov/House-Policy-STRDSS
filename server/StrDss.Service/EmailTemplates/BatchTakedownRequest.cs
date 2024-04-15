c536ba624483a2c2e87306f851befe19859a91c4
ychung-mot <young-jin.chung@dxcas.com>
2024-04-15 08:39:43 -0700

    chore: template update

--- server/StrDss.Service/EmailTemplates/BatchTakedownRequest.cs
+++ server/StrDss.Service/EmailTemplates/BatchTakedownRequest.cs
@@ -14,7 +14,12 @@ namespace StrDss.Service.EmailTemplates
             Subject = "Takedown Request";
 
             return (Preview ? GetPreviewContent() : "") + $@"
-<b>The platform is required to remove the listing within 8 days of the date it was delivered. If the platform fails to remove the listing, local governments can escalate the matter to the Director of the Provincial STR Compliance and Enforcement Unit at: CEUescalations@gov.bc.ca.<br/><br/>
+The short-term rental listings in the attached file are the subject of a request by a local government described under s. 18(3) of the <i>Short-Term Rental Accommodations Act</i> and s. 16 of the <i>Short-Term Rental Accommodations Regulation</i>. <br/><br/>
+A “takedown request” (a request for a platform to cease providing platform services, e.g., removing a listing) has been submitted for each of these listings by the respective local government via the Province of British Columbia’s Short-term Rental (STR) Data Portal.<br/><br/>
+In accordance with s. 18(3) of the <i>Short-Term Rental Accommodations Act</i> and s. 16 (3) of the <i>Short-Term Rental Accommodations Regulation</i>, please cease providing platform services in respect of the attached platform offers within 5 days from the date of receipt of this request.<br/><br/>
+Failure to comply with this request could result in enforcement actions or penalties under the <i>Short-Term Rental Accommodations Act</i>.<br/><br/>
+For more information on these requests, or local government short-term rental business licences, please contact the local government.<br/><br/>
+For more information on the <i>Short-term Rental Accommodations Act</i>, please visit: <a href='https://www2.gov.bc.ca/gov/content/housing-tenancy/short-term-rentals'>New rules for short-term rentals - Province of British Columbia (gov.bc.ca)</a><br/><br/>
 This email has been automatically generated. Please do not reply to this email.
 ";
         }

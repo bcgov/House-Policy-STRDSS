029a7e6b06e58de79b41bbeb5bd533177ea818b0
ychung-mot <young-jin.chung@dxcas.com>
2024-04-15 08:22:44 -0700

    chore: takedown request confirmation template

--- server/StrDss.Service/EmailTemplates/TakedownRequest.cs
+++ server/StrDss.Service/EmailTemplates/TakedownRequest.cs
@@ -16,10 +16,10 @@ namespace StrDss.Service.EmailTemplates
             Subject = "Confirmation of Takedown Request";
 
             return (Preview ? GetPreviewContent() : "") + $@"
-<b>A takedown request for the following short-term rental listing was submitted to the STR Data Portal and will be delivered to the respective short-term rental platform at 11:50pm PST tonight:</b><br/><br/>
+A takedown request for the following short-term rental listing was submitted to the Province of B.C.’s Short-term Rental Data Portal and will be delivered to the platform at 11:50pm PST tonight:<br/><br/>
 <b>{Url}</b><br/><br/>
 Listing ID Number: <b>{ListingId}</b><br/><br/>
-The platform is required to remove the listing within 8 days of the date it was delivered. If the platform fails to remove the listing, local governments can escalate the matter to the Director of the Provincial STR Compliance and Enforcement Unit at: CEUescalations@gov.bc.ca.<br/><br/>
+Under the <a href='https://www.bclaws.gov.bc.ca/civix/document/id/bills/billsprevious/4th42nd:gov35-1'>Short-Term Rental Accommodations Act</a> and its regulations, the platform is required to comply with the request within 5 days from the date of receipt of the request. If the platform fails to comply with the request (e.g., remove the listing), local governments can escalate the matter to the Director of the Provincial STR Compliance and Enforcement Unit at: <a href='mailto: CEUescalations@gov.bc.ca'>CEUescalations@gov.bc.ca</a>.<br/><br/>
 This email has been automatically generated. Please do not reply to this email.
 ";
         }

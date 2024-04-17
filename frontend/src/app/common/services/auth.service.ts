41bd58e06619638db2d7cc5ae126b8c5be5695cc
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-04-17 09:47:27 -0500

    DSS-300 - log off

--- frontend/src/app/common/services/auth.service.ts
+++ frontend/src/app/common/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
-import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,10 @@ export class AuthService {
 
   constructor(private keycloak: KeycloakService) { }
 
-  logout(): Promise<any> {
-    return this.keycloak.logout('/')
+  logout(): void {
+    const redirectUri = `${environment.SM_LOGOFF_URL}?returl=${encodeURIComponent(window.location.origin)}&retnow=1`;
+    const logoffUri = `${this.keycloak.getKeycloakInstance().authServerUrl}/realms/${this.keycloak.getKeycloakInstance().realm
+      }/protocol/openid-connect/logout?redirect_uri=${encodeURIComponent(redirectUri)}`;
+    window.location.href = logoffUri;
   }
 }

2051035a2e68c6836e66c4a19e6bbfe04eb5871d
ychung-mot <young-jin.chung@dxcas.com>
2024-04-19 13:07:16 -0700

    chore: frontend env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     SSO_CLIENT: 'str-dss-5303',
     SSO_REALM: 'standard',
-    SSO_HOST: 'https://dev.loginproxy.gov.bc.ca/auth',
-    ENV_NAME: 'dev',
+    SSO_HOST: '__SSO_HOST__',
+    ENV_NAME: '__ENV_NAME__',
     VERSION: '0.1',
-    SM_LOGOFF_URL: 'https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi/%22',
+    SM_LOGOFF_URL: '__SM_LOGOFF_URL__',
     API_HOST: '/api',
 };

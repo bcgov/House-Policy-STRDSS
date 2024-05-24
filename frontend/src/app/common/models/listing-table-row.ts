26147276ccc71b3b94c59ad57c20e15d22971874
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-05-24 14:36:43 -0500

    Integrated with the backend

--- frontend/src/app/common/models/listing-table-row.ts
+++ frontend/src/app/common/models/listing-table-row.ts
@@ -1,14 +1,41 @@
 export interface ListingTableRow {
-    id: number;
-    status: string;
-    platformName: string;
-    platformId: string;
-    listingId: string;
-    addressRaw: string;
-    addressNormalized: string;
-    entireUnit: boolean;
-    nightsStayed: number;
-    license: string;
-    lastAction: string;
-    lastActionDate: string;
+    rentalListingId: number;
+    listingStatusType: string;
+    offeringOrganizationNm: string;
+    platformListingNo: string;
+    originalAddressTxt: string;
+    matchAddressTxt: string;
+    isEntireUnit: boolean;
+    nightsBookedYtdQty: number;
+    businessLicenceNo: string;
+    lastActionNm: string;
+    lastActionDtm: string;
+
+    offeringOrganizationId?: number;
+    listingStatusSortNo?: number;
+    latestReportPeriodYm?: string;
+    isActive?: boolean;
+    isNew?: boolean;
+    isTakenDown?: boolean;
+    platformListingUrl?: string;
+    matchScoreAmt?: number;
+    addressSort1ProvinceCd?: string;
+    addressSort2LocalityNm?: string;
+    addressSort3LocalityTypeDsc?: string;
+    addressSort4StreetNm?: string;
+    addressSort5StreetTypeDsc?: string;
+    addressSort6StreetDirectionDsc?: string;
+    addressSort7CivicNo?: string;
+    addressSort8UnitNo?: string;
+    listingContactNamesTxt?: string;
+    managingOrganizationId?: number;
+    managingOrganizationNm?: string;
+    isPrincipalResidenceRequired?: boolean;
+    isBusinessLicenceRequired?: boolean;
+    availableBedroomsQty?: number;
+    separateReservationsYtdQty?: number
+    bcRegistryNo?: string;
+
+    rentalListingContacts?: [];
 }
+

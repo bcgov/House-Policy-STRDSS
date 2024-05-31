78c118e083137f7fdb167f4686d5e070ecc177b3
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-05-31 15:06:11 -0500

    DSS-453: Listing file upload: Improving User Experience During Large File Uploads

---
+++ frontend/src/app/common/services/global-loader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalLoaderService {
+  loadingNotification = new Subject<{ isLoading: boolean, title?: string }>()
+
+  constructor() { }
+
+  loadingStart(title: string): void {
+    this.loadingNotification.next({ isLoading: true, title });
+  }
+
+  loadingEnd(): void {
+    this.loadingNotification.next({ isLoading: false, title: '' });
+  }
+}

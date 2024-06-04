df4bbafb208278cc9c230f5883155f5282136164
ychung-mot <young-jin.chung@dxcas.com>
2024-06-04 09:14:05 -0700

    chore: performance

---
+++ server/StrDss.Model/UploadLineToProcess.cs
@@ -0,0 +1,8 @@
+﻿namespace StrDss.Model
+{
+    public class UploadLineToProcess
+    {
+        public string OrgCd { get; set; } = "";
+        public string ListingId { get; set; } = "";
+    }
+}

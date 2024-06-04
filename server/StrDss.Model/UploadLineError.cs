dfb54d7ef95a97d9faa44c5e73bfbbba9412eef4
ychung-mot <young-jin.chung@dxcas.com>
2024-06-04 11:19:04 -0700

    fix(dss-465)

---
+++ server/StrDss.Model/UploadLineError.cs
@@ -0,0 +1,8 @@
+﻿namespace StrDss.Model
+{
+    public class UploadLineError
+    {
+        public string LineText { get; set; } = "";
+        public string? ErrorText { get; set; } = "";
+    }
+}

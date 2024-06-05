f8e3cb1de3cff6159284b1c21366f24e7680e41e
ychung-mot <young-jin.chung@dxcas.com>
2024-06-05 12:46:35 -0700

    feat(dss-423)

---
+++ server/StrDss.Model/RentalReportDtos/RentalListingForTakedownNotice.cs
@@ -0,0 +1,15 @@
+﻿namespace StrDss.Model.RentalReportDtos
+{
+    public class RentalListingForTakedownNotice
+    {
+        public long RentalListingId { get; set; }
+        public string OrganizationCd { get; set; } = "";
+        public string PlatformListingNo { get; set; } = "";
+        public string? PlatformListingUrl { get; set; }
+        public long OfferingPlatformId { get; set; }
+        public long ProvidingPlatformId { get; set; }
+        public List<string> HostEmails { get; set; } = new List<string>();
+        public List<string> PlatformEmails { get; set; } = new List<string>();
+
+    }
+}

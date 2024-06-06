7ed131e2e8f2bd4b3531029ef1a74988189138e3
ychung-mot <young-jin.chung@dxcas.com>
2024-06-06 08:40:21 -0700

    chore: bulk TN - error handling

--- server/StrDss.Service/EmailTemplates/TakedownNotice.cs
+++ server/StrDss.Service/EmailTemplates/TakedownNotice.cs
@@ -15,7 +15,8 @@ namespace StrDss.Service.EmailTemplates
         public string? ListingId { get; set; }
         public string Comment { get; set; } = "";
         public string LgName { get; set; } = "";
-        public long RentalListingId { get; set; }
+        public string OrgCd { get; set; } //optional field
+        public long RentalListingId { get; set; } //optional field
 
         public override string GetContent()
         {

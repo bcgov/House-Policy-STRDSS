4d5287e1b3ff562b4a12de6816c8a73857b79918
ychung-mot <young-jin.chung@dxcas.com>
2024-06-17 14:45:12 -0700

    chore: db scaffolding

---
+++ server/StrDss.Api/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StrDss.Common;
+using StrDss.Model;
+
+namespace StrDss.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : BaseApiController
+    {
+        public RolesController(ICurrentUser currentUser, IMapper mapper, IConfiguration config, ILogger<StrDssLogger> logger) 
+            : base(currentUser, mapper, config, logger)
+        {
+        }
+    }
+}

b30ccb8747f7e4e69b6659c9716571c6423c0bda
ychung-mot <young-jin.chung@dxcas.com>
2024-06-18 08:39:53 -0700

    chore: roles and permission

---
+++ server/StrDss.Data/Repositories/RoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using StrDss.Common;
+using StrDss.Data.Entities;
+using StrDss.Model;
+
+namespace StrDss.Data.Repositories
+{
+    public interface IRoleRepository
+    {
+
+    }
+    public class RoleRepository : RepositoryBase<DssUserRole>, IRoleRepository
+    {
+        public RoleRepository(DssDbContext dbContext, IMapper mapper, ICurrentUser currentUser, ILogger<StrDssLogger> logger) 
+            : base(dbContext, mapper, currentUser, logger)
+        {
+        }
+    }
+}

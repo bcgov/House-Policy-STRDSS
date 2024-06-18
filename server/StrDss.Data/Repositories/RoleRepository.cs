db1de730ab9910dc7ca7380dd5b1b8ab2d2cac09
ychung-mot <young-jin.chung@dxcas.com>
2024-06-18 15:10:28 -0700

    chore: update role

--- server/StrDss.Data/Repositories/RoleRepository.cs
+++ server/StrDss.Data/Repositories/RoleRepository.cs
@@ -5,6 +5,7 @@ using StrDss.Common;
 using StrDss.Data.Entities;
 using StrDss.Model;
 using StrDss.Model.UserDtos;
+using System.Security;
 
 namespace StrDss.Data.Repositories
 {
@@ -13,6 +14,10 @@ namespace StrDss.Data.Repositories
         Task<List<RoleDto>> GetRolesAync();
         Task<List<PermissionDto>> GetPermissionsAync();
         Task<RoleDto?> GetRoleAync(string roleCd);
+        Task CreateRoleAsync(RoleUpdateDto role);
+        Task<int> CountActivePermissionIdsAsnyc(IEnumerable<string> permissions);
+        Task<bool> DoesRoleCdExist(string roleCd);
+        Task UpdateRoleAsync(RoleUpdateDto role);
     }
     public class RoleRepository : RepositoryBase<DssUserRole>, IRoleRepository
     {
@@ -47,6 +52,73 @@ namespace StrDss.Data.Repositories
         public async Task<List<PermissionDto>> GetPermissionsAync()
         {
             return _mapper.Map<List<PermissionDto>>(await _dbContext.DssUserPrivileges.AsNoTracking().ToListAsync());
+        }
+
+        public async Task<int> CountActivePermissionIdsAsnyc(IEnumerable<string> permissions)
+        {
+            return await _dbContext.DssUserPrivileges.CountAsync(x => permissions.Contains(x.UserPrivilegeCd));
+        }
+
+        public async Task CreateRoleAsync(RoleUpdateDto role)
+        {
+            var roleEntity = _mapper.Map<DssUserRole>(role);
+
+            await _dbSet.AddAsync(roleEntity);
+
+            foreach (var permission in role.Permissions)
+            {
+                var privilege = await _dbContext.DssUserPrivileges
+                    .FirstAsync(x => x.UserPrivilegeCd == permission);
+
+                roleEntity.UserPrivilegeCds.Add(privilege);
+            }
+        }
+
+        public async Task<bool> DoesRoleCdExist(string roleCd)
+        {
+            return await _dbSet.AnyAsync(x => x.UserRoleCd == roleCd);
+        }
+
+        public async Task UpdateRoleAsync(RoleUpdateDto role)
+        {
+            var roleEntity = await _dbSet.Include(x => x.UserPrivilegeCds).FirstAsync(x => x.UserRoleCd == role.UserRoleCd);
+
+            _mapper.Map(role, roleEntity);
+
+            roleEntity.UserPrivilegeCds.Clear();
+
+            var permissions = role.Permissions.Distinct();
+            
+            foreach (var permission in role.Permissions)
+            {
+                var privilege = await _dbContext.DssUserPrivileges
+                    .FirstAsync(x => x.UserPrivilegeCd == permission);
+
+                roleEntity.UserPrivilegeCds.Add(privilege);
+            }
+        }
+
+        private async Task SyncPermissionsAsync(RoleUpdateDto role, DssUserRole roleEntity)
+        {
+            var permissionsToDelete =
+                roleEntity.UserPrivilegeCds.Where(x => !role.Permissions.Contains(x.UserPrivilegeCd)).ToList();
+
+            for (var i = permissionsToDelete.Count() - 1; i >= 0; i--)
+            {
+                _dbContext.Remove(permissionsToDelete[i]);
+            }
+
+            var existingPermissions = roleEntity.UserPrivilegeCds.Select(x => x.UserPrivilegeCd);
+
+            var newPermissions = role.Permissions.Where(x => !existingPermissions.Contains(x));
+
+            foreach (var permission in newPermissions)
+            {
+                var privilege = await _dbContext.DssUserPrivileges
+                    .FirstAsync(x => x.UserPrivilegeCd == permission);
+
+                roleEntity.UserPrivilegeCds.Add(privilege);
+            }
         }
     }
 }

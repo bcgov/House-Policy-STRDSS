14d02b532dc53059e1226c753b34ba23f40496eb
Oleksandr Bohuslavskyi <o.bohuslavskyi@dxc.com>
2024-06-20 15:51:42 -0500

    DSS-532: Add spinner bars when system is loading

--- frontend/src/app/features/components/dashboard/dashboard.component.ts
+++ frontend/src/app/features/components/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ import { DashboardService } from '../../../common/services/dashboard.service';
 import { DashboardCard } from '../../../common/models/dashboard-card';
 import { ListingUploadHistoryTableComponent } from '../../../common/listing-upload-history-table/listing-upload-history-table.component';
 import { listing_file_upload } from '../../../common/consts/permissions.const';
+import { GlobalLoaderService } from '../../../common/services/global-loader.service';
 
 
 @Component({
@@ -34,16 +35,21 @@ export class DashboardComponent implements OnInit {
   constructor(
     private router: Router,
     private userDataService: UserDataService,
-    private dashboardService: DashboardService
+    private dashboardService: DashboardService,
+    private loaderService: GlobalLoaderService,
   ) { }
 
   ngOnInit(): void {
+    this.loaderService.loadingStart();
     this.userDataService.getCurrentUser().subscribe({
       next: (value: User) => {
         this.currentUser = value;
         this.cardsToDisplay = this.dashboardService.getCardsPerUserType(this.currentUser);
         this.showListingHistory = this.currentUser.permissions.includes(listing_file_upload);
       },
+      complete: () => {
+        this.loaderService.loadingEnd();
+      }
     })
   }
 

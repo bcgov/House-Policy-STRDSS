96299898a6df3bef26aa2733322bdc008b12e7e8
ychung-mot <young-jin.chung@dxcas.com>
2024-06-26 09:48:21 -0700

    feat(dss-19)

--- server/StrDss.Service/RentalListingService.cs
+++ server/StrDss.Service/RentalListingService.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using StrDss.Common;
 using StrDss.Data;
+using StrDss.Data.Entities;
 using StrDss.Data.Repositories;
 using StrDss.Model;
 using StrDss.Model.RentalReportDtos;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace StrDss.Service
@@ -59,6 +61,47 @@ namespace StrDss.Service
         public async Task<RentalListingViewDto?> GetRentalListing(long rentalListingId)
         {
             return await _listingRepo.GetRentalListing(rentalListingId);
+        }
+
+        //listing IDs order by ManagingOrganizationId, IsPrincipalResidenceRequired
+        //List<string>, lg, all, pr
+        //loop through listings
+        //fetch listing, monthly data, action history, etc for the line
+        //if lg ends, creates lg zip and insert to DB and clear lg
+        //after loop, creates all zip and pr zip and insert to dB.
+        private string GenerateCsv(IEnumerable<DssRentalListingVw> rentalListings)
+        {
+            var csv = new StringBuilder();
+            csv.AppendLine("RentalListingId,ListingStatusType,ListingStatusSortNo,LatestReportPeriodYm,IsActive,IsNew,IsTakenDown,OfferingOrganizationId,OfferingOrganizationCd,OfferingOrganizationNm,PlatformListingNo,PlatformListingUrl,OriginalAddressTxt,MatchScoreAmt,MatchAddressTxt,AddressSort1ProvinceCd,AddressSort2LocalityNm,AddressSort3LocalityTypeDsc,AddressSort4StreetNm,AddressSort5StreetTypeDsc,AddressSort6StreetDirectionDsc,AddressSort7CivicNo,AddressSort8UnitNo,ListingContactNamesTxt,ManagingOrganizationId,ManagingOrganizationNm,IsPrincipalResidenceRequired,IsBusinessLicenceRequired,IsEntireUnit,AvailableBedroomsQty,NightsBookedYtdQty,SeparateReservationsYtdQty,BusinessLicenceNo,BcRegistryNo,LastActionNm,LastActionDtm");
+
+            foreach (var listing in rentalListings)
+            {
+                csv.AppendLine($"{EscapeCsvField(listing.RentalListingId)},{EscapeCsvField(listing.ListingStatusType)},{EscapeCsvField(listing.ListingStatusSortNo)},{EscapeCsvField(listing.LatestReportPeriodYm)},{EscapeCsvField(listing.IsActive)},{EscapeCsvField(listing.IsNew)},{EscapeCsvField(listing.IsTakenDown)},{EscapeCsvField(listing.OfferingOrganizationId)},{EscapeCsvField(listing.OfferingOrganizationCd)},{EscapeCsvField(listing.OfferingOrganizationNm)},{EscapeCsvField(listing.PlatformListingNo)},{EscapeCsvField(listing.PlatformListingUrl)},{EscapeCsvField(listing.OriginalAddressTxt)},{EscapeCsvField(listing.MatchScoreAmt)},{EscapeCsvField(listing.MatchAddressTxt)},{EscapeCsvField(listing.AddressSort1ProvinceCd)},{EscapeCsvField(listing.AddressSort2LocalityNm)},{EscapeCsvField(listing.AddressSort3LocalityTypeDsc)},{EscapeCsvField(listing.AddressSort4StreetNm)},{EscapeCsvField(listing.AddressSort5StreetTypeDsc)},{EscapeCsvField(listing.AddressSort6StreetDirectionDsc)},{EscapeCsvField(listing.AddressSort7CivicNo)},{EscapeCsvField(listing.AddressSort8UnitNo)},{EscapeCsvField(listing.ListingContactNamesTxt)},{EscapeCsvField(listing.ManagingOrganizationId)},{EscapeCsvField(listing.ManagingOrganizationNm)},{EscapeCsvField(listing.IsPrincipalResidenceRequired)},{EscapeCsvField(listing.IsBusinessLicenceRequired)},{EscapeCsvField(listing.IsEntireUnit)},{EscapeCsvField(listing.AvailableBedroomsQty)},{EscapeCsvField(listing.NightsBookedYtdQty)},{EscapeCsvField(listing.SeparateReservationsYtdQty)},{EscapeCsvField(listing.BusinessLicenceNo)},{EscapeCsvField(listing.BcRegistryNo)},{EscapeCsvField(listing.LastActionNm)},{EscapeCsvField(listing.LastActionDtm)}");
+            }
+
+            return csv.ToString();
+        }
+
+        private static string EscapeCsvField(object? field)
+        {
+            if (field == null)
+            {
+                return string.Empty;
+            }
+
+            var fieldString = field.ToString();
+
+            if (fieldString!.Contains('"'))
+            {
+                fieldString = fieldString.Replace("\"", "\"\"");
+            }
+
+            if (fieldString.Contains(',') || fieldString.Contains('\n') || fieldString.Contains('\r'))
+            {
+                fieldString = $"\"{fieldString}\"";
+            }
+
+            return fieldString;
         }
     }
 }

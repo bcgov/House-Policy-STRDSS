ba83326d6789ed48aa477ddf6551785afdab5fa5
ychung-mot <young-jin.chung@dxcas.com>
2024-07-02 06:54:55 -0700

    fix: update isEnabled

--- server/StrDss.Model/UserDtos/UserUpdateDto.cs
+++ server/StrDss.Model/UserDtos/UserUpdateDto.cs
@@ -5,6 +5,7 @@
         public long UserIdentityId { get; set; }
         public long RepresentedByOrganizationId { get; set; }
         public List<string> RoleCds { get; set; } = new List<string>();
+        public bool IsEnabled { get; set; }
         public DateTime? UpdDtm { get; set; }
     }
 }

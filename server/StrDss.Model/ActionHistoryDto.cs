cac0287f0409bd75fef533242727d0d43959d5db
ychung-mot <young-jin.chung@dxcas.com>
2024-07-02 09:43:15 -0700

    chore: action history

--- server/StrDss.Model/ActionHistoryDto.cs
+++ server/StrDss.Model/ActionHistoryDto.cs
@@ -4,7 +4,7 @@
     {
         public string Action { get; set; } = "";
         public DateTime? Date { get; set; }
-        public string? User { get; set; };
+        public string? User { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
     }

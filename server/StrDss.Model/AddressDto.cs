685d026478f91f7d005a22bd834eda1213677703
ychung-mot <young-jin.chung@dxcas.com>
2024-07-04 07:56:39 -0700

    chore: address candidates

---
+++ server/StrDss.Model/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StrDss.Model
+{
+    public class AddressDto
+    {
+        public string Address { get; set; } = "";
+        [JsonIgnore]
+        public NetTopologySuite.Geometries.Point LocationGeometry { get; set; }
+        public long? OrganizationId { get; set; }
+
+    }
+}

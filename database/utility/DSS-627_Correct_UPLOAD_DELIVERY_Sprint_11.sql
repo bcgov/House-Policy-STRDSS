de88c8b9c25fe0923e13509d093b8404815c4ad1
Leif Larsen <leif.larsen@dxcas.com>
2024-08-02 16:11:59 -0700

    DSS-627 DB: Support takedown confirmation for unreported listing nos

--- database/utility/DSS-627_Correct_UPLOAD_DELIVERY_Sprint_11.sql
+++ database/utility/DSS-627_Correct_UPLOAD_DELIVERY_Sprint_11.sql
@@ -1,7 +1,9 @@
-UPDATE dss_upload_delivery
-set upload_delivery_type='rental_report'
-where upload_delivery_type is null;
+alter table dss_upload_delivery
+disable trigger all;
 
 UPDATE dss_upload_delivery
 set upload_delivery_type='Listing Data'
 where upload_delivery_type = 'rental_report';
+
+alter table dss_upload_delivery
+enable trigger all;

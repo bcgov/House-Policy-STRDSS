8dd73fe92b352494dbbbcce1804980de8d9e1a39
ychung-mot <young-jin.chung@dxcas.com>
2024-08-19 15:34:33 -0700

    chore: biz info

--- server/StrDss.Model/RentalReportDtos/RentalListingViewDto.cs
+++ server/StrDss.Model/RentalReportDtos/RentalListingViewDto.cs
@@ -83,7 +83,7 @@ namespace StrDss.Model.RentalReportDtos
         public DateTime? LastActionDtm { get; set; }
         public bool HasAtLeastOneValidHostEmail { get; set; }
         public List<HostInfo> HostsInfo { get; set; } = new List<HostInfo>();
-        public BizLicenseDto? BizLicenseDto { get; set; }
+        public BizLicenseDto? BizLicenseInfo { get; set; }
         public virtual ICollection<RentalListingContactDto> Hosts { get; set; } = new List<RentalListingContactDto>();
         public virtual ICollection<ListingHistoryDto> ListingHistory { get; set; } = new List<ListingHistoryDto>();
         public virtual ICollection<ActionHistoryDto> ActionHistory { get; set; } = new List<ActionHistoryDto>();

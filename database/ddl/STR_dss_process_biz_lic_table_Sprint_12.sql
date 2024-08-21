12469377fcb8c6524efd5b84681bf334481b0d75
Leif Larsen <leif.larsen@dxcas.com>
2024-08-20 17:06:40 -0700

    DSS-479 Database updates for BL data upload

--- database/ddl/STR_dss_process_biz_lic_table_Sprint_12.sql
+++ database/ddl/STR_dss_process_biz_lic_table_Sprint_12.sql
@@ -2,65 +2,111 @@ CREATE OR REPLACE PROCEDURE dss_process_biz_lic_table()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-
     -- Deletion 
-	
-	
+	DELETE FROM dss_business_licence
+	WHERE providing_organization_id IN (SELECT providing_organization_id FROM biz_lic_table)
+	  AND business_licence_no NOT IN (SELECT business_licence_no FROM biz_lic_table);
+
     -- Insert into dss_business_licence from biz_lic_table or update if exists
-    INSERT INTO public.dss_business_licence (
+	MERGE INTO dss_business_licence AS tgt
+	USING (SELECT * FROM biz_lic_table) AS src
+	ON (tgt.providing_organization_id=src.providing_organization_id
+	AND tgt.business_licence_no=src.business_licence_no)
+    WHEN MATCHED THEN UPDATE SET 
+        expiry_dt = src.expiry_dt,
+        physical_rental_address_txt = src.physical_rental_address_txt,
+        licence_type_txt = src.licence_type_txt,
+        restriction_txt = src.restriction_txt,
+        business_nm = src.business_nm,
+        mailing_street_address_txt = src.mailing_street_address_txt,
+        mailing_city_nm = src.mailing_city_nm,
+        mailing_province_cd = src.mailing_province_cd,
+        mailing_postal_cd = src.mailing_postal_cd,
+        business_owner_nm = src.business_owner_nm,
+        business_owner_phone_no = src.business_owner_phone_no,
+        business_owner_email_address_dsc = src.business_owner_email_address_dsc,
+        business_operator_nm = src.business_operator_nm,
+        business_operator_phone_no = src.business_operator_phone_no,
+        business_operator_email_address_dsc = src.business_operator_email_address_dsc,
+        infraction_txt = src.infraction_txt,
+        infraction_dt = src.infraction_dt,
+        property_zone_txt = src.property_zone_txt,
+        available_bedrooms_qty = src.available_bedrooms_qty,
+        max_guests_allowed_qty = src.max_guests_allowed_qty,
+        is_principal_residence = src.is_principal_residence,
+        is_owner_living_onsite = src.is_owner_living_onsite,
+        is_owner_property_tenant = src.is_owner_property_tenant,
+        property_folio_no = src.property_folio_no,
+        property_parcel_identifier_no = src.property_parcel_identifier_no,
+        property_legal_description_txt = src.property_legal_description_txt,
+        licence_status_type = src.licence_status_type
+	WHEN NOT MATCHED THEN INSERT (
         business_licence_no, expiry_dt, physical_rental_address_txt, licence_type_txt, restriction_txt, 
         business_nm, mailing_street_address_txt, mailing_city_nm, mailing_province_cd, mailing_postal_cd, 
         business_owner_nm, business_owner_phone_no, business_owner_email_address_dsc, business_operator_nm, 
         business_operator_phone_no, business_operator_email_address_dsc, infraction_txt, infraction_dt, 
         property_zone_txt, available_bedrooms_qty, max_guests_allowed_qty, is_principal_residence, 
         is_owner_living_onsite, is_owner_property_tenant, property_folio_no, property_parcel_identifier_no, 
-        property_legal_description_txt, licence_status_type, providing_organization_id, upd_dtm
-    )
-    SELECT 
+        property_legal_description_txt, licence_status_type, providing_organization_id)
+    VALUES (
+        src.business_licence_no, src.expiry_dt, src.physical_rental_address_txt, src.licence_type_txt, src.restriction_txt,
+        src.business_nm, src.mailing_street_address_txt, src.mailing_city_nm, src.mailing_province_cd, src.mailing_postal_cd,
+        src.business_owner_nm, src.business_owner_phone_no, src.business_owner_email_address_dsc, src.business_operator_nm,
+        src.business_operator_phone_no, src.business_operator_email_address_dsc, src.infraction_txt, src.infraction_dt,
+        src.property_zone_txt, src.available_bedrooms_qty, src.max_guests_allowed_qty, src.is_principal_residence,
+        src.is_owner_living_onsite, src.is_owner_property_tenant, src.property_folio_no, src.property_parcel_identifier_no,
+        src.property_legal_description_txt, src.licence_status_type, src.providing_organization_id);
+/*
+    -- Update dss_rental_listing if new match found
+	MERGE INTO dss_rental_listing AS tgt
+	USING (SELECT * FROM biz_lic_table) AS src
+	ON (tgt.providing_organization_id=src.providing_organization_id
+	AND tgt.business_licence_no=src.business_licence_no)
+    WHEN MATCHED THEN UPDATE SET 
+        expiry_dt = src.expiry_dt,
+        physical_rental_address_txt = src.physical_rental_address_txt,
+        licence_type_txt = src.licence_type_txt,
+        restriction_txt = src.restriction_txt,
+        business_nm = src.business_nm,
+        mailing_street_address_txt = src.mailing_street_address_txt,
+        mailing_city_nm = src.mailing_city_nm,
+        mailing_province_cd = src.mailing_province_cd,
+        mailing_postal_cd = src.mailing_postal_cd,
+        business_owner_nm = src.business_owner_nm,
+        business_owner_phone_no = src.business_owner_phone_no,
+        business_owner_email_address_dsc = src.business_owner_email_address_dsc,
+        business_operator_nm = src.business_operator_nm,
+        business_operator_phone_no = src.business_operator_phone_no,
+        business_operator_email_address_dsc = src.business_operator_email_address_dsc,
+        infraction_txt = src.infraction_txt,
+        infraction_dt = src.infraction_dt,
+        property_zone_txt = src.property_zone_txt,
+        available_bedrooms_qty = src.available_bedrooms_qty,
+        max_guests_allowed_qty = src.max_guests_allowed_qty,
+        is_principal_residence = src.is_principal_residence,
+        is_owner_living_onsite = src.is_owner_living_onsite,
+        is_owner_property_tenant = src.is_owner_property_tenant,
+        property_folio_no = src.property_folio_no,
+        property_parcel_identifier_no = src.property_parcel_identifier_no,
+        property_legal_description_txt = src.property_legal_description_txt,
+        licence_status_type = src.licence_status_type
+	WHEN NOT MATCHED THEN INSERT (
         business_licence_no, expiry_dt, physical_rental_address_txt, licence_type_txt, restriction_txt, 
         business_nm, mailing_street_address_txt, mailing_city_nm, mailing_province_cd, mailing_postal_cd, 
         business_owner_nm, business_owner_phone_no, business_owner_email_address_dsc, business_operator_nm, 
         business_operator_phone_no, business_operator_email_address_dsc, infraction_txt, infraction_dt, 
-        property_zone_txt, available_bedrooms_qty, max_guests_allowed_qty, 
-        CASE WHEN is_principal_residence = 'Y' THEN TRUE ELSE FALSE END, -- Convert 'Y'/'N' to boolean
-        CASE WHEN is_owner_living_onsite = 'Y' THEN TRUE ELSE FALSE END, -- Convert 'Y'/'N' to boolean
-        CASE WHEN is_owner_property_tenant = 'Y' THEN TRUE ELSE FALSE END, -- Convert 'Y'/'N' to boolean
-        property_folio_no, property_parcel_identifier_no, property_legal_description_txt, 
-        licence_status_type, providing_organization_id,
-        NOW() -- Set the current timestamp for upd_dtm
-    FROM biz_lic_table
-    ON CONFLICT (providing_organization_id, business_licence_no)
-    DO UPDATE
-    SET 
-        expiry_dt = EXCLUDED.expiry_dt,
-        physical_rental_address_txt = EXCLUDED.physical_rental_address_txt,
-        licence_type_txt = EXCLUDED.licence_type_txt,
-        restriction_txt = EXCLUDED.restriction_txt,
-        business_nm = EXCLUDED.business_nm,
-        mailing_street_address_txt = EXCLUDED.mailing_street_address_txt,
-        mailing_city_nm = EXCLUDED.mailing_city_nm,
-        mailing_province_cd = EXCLUDED.mailing_province_cd,
-        mailing_postal_cd = EXCLUDED.mailing_postal_cd,
-        business_owner_nm = EXCLUDED.business_owner_nm,
-        business_owner_phone_no = EXCLUDED.business_owner_phone_no,
-        business_owner_email_address_dsc = EXCLUDED.business_owner_email_address_dsc,
-        business_operator_nm = EXCLUDED.business_operator_nm,
-        business_operator_phone_no = EXCLUDED.business_operator_phone_no,
-        business_operator_email_address_dsc = EXCLUDED.business_operator_email_address_dsc,
-        infraction_txt = EXCLUDED.infraction_txt,
-        infraction_dt = EXCLUDED.infraction_dt,
-        property_zone_txt = EXCLUDED.property_zone_txt,
-        available_bedrooms_qty = EXCLUDED.available_bedrooms_qty,
-        max_guests_allowed_qty = EXCLUDED.max_guests_allowed_qty,
-        is_principal_residence = EXCLUDED.is_principal_residence,
-        is_owner_living_onsite = EXCLUDED.is_owner_living_onsite,
-        is_owner_property_tenant = EXCLUDED.is_owner_property_tenant,
-        property_folio_no = EXCLUDED.property_folio_no,
-        property_parcel_identifier_no = EXCLUDED.property_parcel_identifier_no,
-        property_legal_description_txt = EXCLUDED.property_legal_description_txt,
-        licence_status_type = EXCLUDED.licence_status_type,
-        upd_dtm = NOW(); -- Update the update timestamp
-    
+        property_zone_txt, available_bedrooms_qty, max_guests_allowed_qty, is_principal_residence, 
+        is_owner_living_onsite, is_owner_property_tenant, property_folio_no, property_parcel_identifier_no, 
+        property_legal_description_txt, licence_status_type, providing_organization_id)
+    VALUES (
+        src.business_licence_no, src.expiry_dt, src.physical_rental_address_txt, src.licence_type_txt, src.restriction_txt,
+        src.business_nm, src.mailing_street_address_txt, src.mailing_city_nm, src.mailing_province_cd, src.mailing_postal_cd,
+        src.business_owner_nm, src.business_owner_phone_no, src.business_owner_email_address_dsc, src.business_operator_nm,
+        src.business_operator_phone_no, src.business_operator_email_address_dsc, src.infraction_txt, src.infraction_dt,
+        src.property_zone_txt, src.available_bedrooms_qty, src.max_guests_allowed_qty, src.is_principal_residence,
+        src.is_owner_living_onsite, src.is_owner_property_tenant, src.property_folio_no, src.property_parcel_identifier_no,
+        src.property_legal_description_txt, src.licence_status_type, src.providing_organization_id);
+*/
     -- Optional: Truncate the temporary table after processing
     TRUNCATE TABLE biz_lic_table;
     

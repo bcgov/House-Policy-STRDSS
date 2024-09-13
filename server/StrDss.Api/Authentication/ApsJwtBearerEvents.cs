41cb7f5e1475d3a5bdd93b75af873f0c10eed1f2
ychung-mot <young-jin.chung@dxcas.com>
2024-09-13 07:05:12 -0700

    chore: gateway

--- server/StrDss.Api/Authentication/ApsJwtBearerEvents.cs
+++ server/StrDss.Api/Authentication/ApsJwtBearerEvents.cs
@@ -25,6 +25,17 @@ namespace StrDss.Api.Authentication
             _memoryCache = memoryCache;
         }
 
+        public override Task AuthenticationFailed(AuthenticationFailedContext context)
+        {
+            var username = context.HttpContext.User?.Identity?.Name ?? "Unknown";
+            var ipAddress = context.HttpContext.Connection.RemoteIpAddress;
+            var ip = ipAddress == null ? "Unknown" : ipAddress.ToString();
+
+            _logger.LogInformation($"[AUTH] Authentication failed for user '{username}' from IP address '{ip}'.");
+
+            return base.AuthenticationFailed(context);
+        }
+
         public override async Task TokenValidated(TokenValidatedContext context)
         {
             _currentUser.LoadApsSession(context!.Principal!);

fb8b52a5b7411a3b8a53b79787929ae1f9d627b8
Rick Anderson <Richand101@hotmail.com>
2024-09-17 21:14:56 -0700

    View Aggregate listings tests added

---
+++ Test/UITest/SpecFlowProjectBDD/Features/ViewAggregatedListings.feature
@@ -0,0 +1,77 @@
+﻿Feature: ViewAggregatedListings
+Link to a feature: https://hous-hpb.atlassian.net/browse/DSS-505
+
+@LandingPage
+Scenario: ViewAggregatedListings 
+#User Authentication
+	#Given that I am an authenticated user "<UserName>" with the necessary permissions to view listings View listings (listing_read)  and the expected result is
+	Given that I am an authenticated user "<UserName>" with the necessary permissions to view listings and the expected result is "<ExpectedResult>" and I am a "<UserType>" user
+
+
+
+#Access to Data Portal:
+
+	When I access the Data Portal
+
+#View Aggregated Listings
+
+	Given that I am on the aggregated listing page
+
+	Then I should see a parent row for each STR “property grouping “with information that is common to all listings associated with the group
+
+	And the ability to view drop down child rows under each parent row  so that I can review all listings that are associated with a property grouping
+
+#Parent Row 
+
+	Given I am viewing a listing on the aggregated listing page
+
+	Then I should see information that is “common” to all listings associated with a property, including:
+
+	#Primary Host name
+	#
+	#Address (Best Match) 
+	#
+	#Nights stayed YTD (from listing data)
+	#
+	#Business licence number
+	#
+	#Last Action
+	#
+	#Last Action Date
+
+	And I should have the option to expand a dropdown to view all child rows with listings associated with the parent row
+
+#Child rows
+
+	When I expand the dropdown to view all child rows,
+
+	Then I should see key information for each listing, including:
+
+	#status/ flags
+	#
+	#platform name
+	#
+	#listing ID
+	#
+	#Link to Listing Details
+	#
+	#Address (Best match)
+	#
+	#Nights stayed (YTD)
+	#
+	#Business Licence 
+	#
+	#last action
+	#
+	#last action date
+
+
+Examples:
+	| UserName     | UserType | Environment | ExpectedResult |
+	| STRDSSLg1Dev | lg_staff | dev         | pass           |
+
+
+
+
+
+
---
+++ Test/UITest/SpecFlowProjectBDD/StepDefinitions/ViewAggregatedListings.cs
@@ -0,0 +1,135 @@
+using Configuration;
+using DataBase.Entities;
+using DataBase.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework.Legacy;
+using SpecFlowProjectBDD.Helpers;
+using System;
+using TechTalk.SpecFlow;
+using TestFrameWork.Models;
+using UITest.PageObjects;
+using UITest.TestDriver;
+
+namespace SpecFlowProjectBDD.StepDefinitions
+{
+    [Binding]
+    public class ViewAggregatedListings
+    {
+        private IDriver _Driver;
+        private LandingPage _LandingPage;
+        private ListingsPage _ListingsPage;
+        private BulkComplianceNoticePage _BulkComplianceNoticePage;
+        private DelistingWarningPage _DelistingWarningPage;
+        private PathFinderPage _PathFinderPage;
+        private BCIDPage _BCIDPage;
+        private NoticeOfTakeDownPage _NoticeOfTakeDownPage;
+        private string _TestUserName;
+        private string _TestPassword;
+        private bool _ExpectedResult = false;
+        private SFEnums.UserTypeEnum _UserType;
+        private SFEnums.LogonTypeEnum? _LogonType;
+        private AppSettings _AppSettings;
+        private DateTime _updateTime;
+        private DssDbContext _DssDBContext;
+        private IUnitOfWork _UnitOfWork;
+        private SFEnums.Environment _Environment = SFEnums.Environment.LOCAL;
+
+        public ViewAggregatedListings(SeleniumDriver Driver)
+        {
+            _Driver = Driver;
+            _LandingPage = new LandingPage(_Driver);
+            _ListingsPage = new ListingsPage(_Driver);
+            _BulkComplianceNoticePage = new BulkComplianceNoticePage(_Driver);
+            _DelistingWarningPage = new DelistingWarningPage(_Driver);
+            _NoticeOfTakeDownPage = new NoticeOfTakeDownPage(_Driver);
+            _PathFinderPage = new PathFinderPage(_Driver);
+            _BCIDPage = new BCIDPage(_Driver);
+            _AppSettings = new AppSettings();
+            DbContextOptions<DssDbContext> dbContextOptions = new DbContextOptions<DssDbContext>();
+            _DssDBContext = new DssDbContext(dbContextOptions);
+
+            string dbConnectionString = _AppSettings.GetConnectionString(_Environment.ToString().ToLower()) ?? string.Empty;
+
+            _DssDBContext = new DssDbContext(dbContextOptions, dbConnectionString);
+            _UnitOfWork = new UnitOfWork(_DssDBContext);
+
+        }
+        //[Given(@"that I am an authenticated user ""(.*)"" with the necessary permissions to view listings View listings (listing_read)  and the expected result is ""(.*)"" and I am a ""(.*)"" user")]
+
+        [Given(@"that I am an authenticated user ""(.*)"" with the necessary permissions to view listings and the expected result is ""(.*)"" and I am a ""(.*)"" user")]
+        public void GivenIAmAauthenticatedLGStaffMemberUser(string UserName, string ExpectedResult = "pass", string UserType = "")
+        {
+            _TestUserName = UserName;
+            _TestPassword = _AppSettings.GetUser(_TestUserName) ?? string.Empty;
+            _ExpectedResult = ExpectedResult.ToUpper() == "PASS" ? true : false;
+
+            _Driver.Url = _AppSettings.GetServer("default");
+            _Driver.Navigate();
+
+            AuthHelper authHelper = new AuthHelper(_Driver);
+            UserHelper userHelper = new UserHelper();
+
+            _UserType = userHelper.SetUserType(UserType);
+            //Authenticate user using IDir or BCID depending on the user
+            _LogonType = authHelper.Authenticate(_TestUserName, _TestPassword, _UserType);
+            ClassicAssert.IsNotNull(_LogonType, "Logon FAILED");
+
+            TermsAndConditionsHelper termsAndConditionsHelper = new TermsAndConditionsHelper(_Driver);
+            termsAndConditionsHelper.HandleTermsAndConditions();
+        }
+
+        [When(@"I access the Data Portal")]
+        public void WhenIAccessTheDataPortal()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Given(@"that I am on the aggregated listing page")]
+        public void GivenThatIAmOnTheAggregatedListingPage()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@"I should see a parent row for each STR “property grouping “with information that is common to all listings associated with the group")]
+        public void ThenIShouldSeeAParentRowForEachSTRPropertyGroupingWithInformationThatIsCommonToAllListingsAssociatedWithTheGroup()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@"the ability to view drop down child rows under each parent row  so that I can review all listings that are associated with a property grouping")]
+        public void ThenTheAbilityToViewDropDownChildRowsUnderEachParentRowSoThatICanReviewAllListingsThatAreAssociatedWithAPropertyGrouping()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Given(@"I am viewing a listing on the aggregated listing page")]
+        public void GivenIAmViewingAListingOnTheAggregatedListingPage()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@"I should see information that is “common” to all listings associated with a property, including:")]
+        public void ThenIShouldSeeInformationThatIsCommonToAllListingsAssociatedWithAPropertyIncluding()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@"I should have the option to expand a dropdown to view all child rows with listings associated with the parent row")]
+        public void ThenIShouldHaveTheOptionToExpandADropdownToViewAllChildRowsWithListingsAssociatedWithTheParentRow()
+        {
+            //throw new PendingStepException();
+        }
+
+        [When(@"I expand the dropdown to view all child rows,")]
+        public void WhenIExpandTheDropdownToViewAllChildRows()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@"I should see key information for each listing, including:")]
+        public void ThenIShouldSeeKeyInformationForEachListingIncluding()
+        {
+            //throw new PendingStepException();
+        }
+    }
+}

fc33a288e2af5a45fc732de4e6ae223df522dbe2
ychung-mot <young-jin.chung@dxcas.com>
2024-09-23 07:35:35 -0700

    chore: grouped listings performance

--- frontend/src/app/common/services/listing-data.service.ts
+++ frontend/src/app/common/services/listing-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { PagingResponse } from '../models/paging-response';
 import { ListingUploadHistoryRecord } from '../models/listing-upload-history-record';
@@ -11,231 +11,272 @@ import { ExportJurisdiction } from '../models/export-listing';
 import { ListingFilter } from '../models/listing-filter';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ListingDataService {
-  httpOptions = {
-    headers: new HttpHeaders({
-      "Content-Type": "multipart/form-data",
-    })
-  };
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'multipart/form-data',
+        }),
+    };
 
-  textHeaders = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    textHeaders = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
 
-  constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) {}
 
-  uploadListings(reportPeriod: string, organizationId: number, file: any): Observable<unknown> {
-    const formData = new FormData();
-    formData.append('reportPeriod', reportPeriod);
-    formData.append('organizationId', organizationId.toString());
-    formData.append('file', file);
+    uploadListings(reportPeriod: string, organizationId: number, file: any): Observable<unknown> {
+        const formData = new FormData();
+        formData.append('reportPeriod', reportPeriod);
+        formData.append('organizationId', organizationId.toString());
+        formData.append('file', file);
 
-    return this.httpClient.post<any>(
-      `${environment.API_HOST}/RentalListingReports`,
-      formData,
-    );
-  }
-
-  uploadTakedown(reportPeriod: string, organizationId: number, file: any): Observable<unknown> {
-    const formData = new FormData();
-    formData.append('reportPeriod', reportPeriod);
-    formData.append('organizationId', organizationId.toString());
-    formData.append('file', file);
-
-    return this.httpClient.post<any>(
-      `${environment.API_HOST}/RentalListings/takedownconfirmation`,
-      formData,
-    );
-  }
-
-  getListingUploadHistoryRecords(
-    pageNumber: number = 1,
-    pageSize: number = 10,
-    platformId: number = 0,
-    orderBy: string = '',
-    direction: 'asc' | 'desc' = 'asc'
-  ): Observable<PagingResponse<ListingUploadHistoryRecord>> {
-    let url = `${environment.API_HOST}/rentallistingreports/rentallistinghistory?pageSize=${pageSize}&pageNumber=${pageNumber}`;
-
-    if (!!platformId) {
-      url += `&platformId=${platformId}`;
+        return this.httpClient.post<any>(`${environment.API_HOST}/RentalListingReports`, formData);
     }
 
-    if (orderBy) {
-      url += `&orderBy=${orderBy}&direction=${direction}`;
+    uploadTakedown(reportPeriod: string, organizationId: number, file: any): Observable<unknown> {
+        const formData = new FormData();
+        formData.append('reportPeriod', reportPeriod);
+        formData.append('organizationId', organizationId.toString());
+        formData.append('file', file);
+
+        return this.httpClient.post<any>(
+            `${environment.API_HOST}/RentalListings/takedownconfirmation`,
+            formData,
+        );
     }
 
-    return this.httpClient.get<PagingResponse<ListingUploadHistoryRecord>>(url);
-  }
+    getListingUploadHistoryRecords(
+        pageNumber: number = 1,
+        pageSize: number = 10,
+        platformId: number = 0,
+        orderBy: string = '',
+        direction: 'asc' | 'desc' = 'asc',
+    ): Observable<PagingResponse<ListingUploadHistoryRecord>> {
+        let url = `${environment.API_HOST}/rentallistingreports/rentallistinghistory?pageSize=${pageSize}&pageNumber=${pageNumber}`;
 
-  getUploadHistoryErrors(id: number): Observable<any> {
-    return this.httpClient.get<any>(`${environment.API_HOST}/rentallistingreports/uploads/${id}/errorfile`,
-      { headers: this.textHeaders, responseType: 'text' as 'json' });
-  }
-
-  getListings(
-    pageNumber: number = 1,
-    pageSize: number = 10,
-    orderBy: string = '',
-    direction: 'asc' | 'desc' = 'asc',
-    searchReq: ListingSearchRequest = {},
-    filter?: ListingFilter
-  ): Observable<PagingResponse<ListingTableRow>> {
-    let endpointUrl = `${environment.API_HOST}/rentallistings?pageSize=${pageSize}&pageNumber=${pageNumber}`;
-
-    if (orderBy) {
-      endpointUrl += `&orderBy=${orderBy}&direction=${direction}`;
-    }
-
-    if (searchReq.all) {
-      endpointUrl += `&all=${searchReq.all}`;
-    }
-    if (searchReq.address) {
-      endpointUrl += `&address=${searchReq.address}`;
-    }
-    if (searchReq.url) {
-      endpointUrl += `&url=${searchReq.url}`;
-    }
-    if (searchReq.listingId) {
-      endpointUrl += `&listingId=${searchReq.listingId}`;
-    }
-    if (searchReq.hostName) {
-      endpointUrl += `&hostName=${searchReq.hostName}`;
-    }
-    if (searchReq.businessLicence) {
-      endpointUrl += `&businessLicence=${searchReq.businessLicence}`;
-    }
-
-    if (filter) {
-      if (filter.byLocation) {
-        if (!!filter.byLocation?.isPrincipalResidenceRequired) {
-          endpointUrl += `&prRequirement=${filter.byLocation.isPrincipalResidenceRequired == 'Yes'}`;
-        }
-        if (!!filter.byLocation?.isBusinessLicenceRequired) {
-          endpointUrl += `&blRequirement=${filter.byLocation.isBusinessLicenceRequired == 'Yes'}`;
-        }
-      }
-      if (filter.byStatus) {
-        if (filter.byStatus.reassigned !== null && filter.byStatus.reassigned !== undefined) {
-          endpointUrl += `&reassigned=${!!filter.byStatus.reassigned}`;
-        }
-        if (filter.byStatus.takedownComplete !== null && filter.byStatus.takedownComplete !== undefined) {
-          endpointUrl += `&takedownComplete=${!!filter.byStatus.takedownComplete}`;
+        if (!!platformId) {
+            url += `&platformId=${platformId}`;
         }
 
-        const statuses = new Array();
-        if (filter.byStatus.active) statuses.push('A')
-        if (filter.byStatus.inactive) statuses.push('I')
-        if (filter.byStatus.new) statuses.push('N')
-
-        if (statuses.length) {
-          endpointUrl += `&statuses=${statuses.join(',')}`;
-        }
-      }
-      if (!!filter.community) {
-        endpointUrl += `&lgId=${filter.community}`;
-      }
-    }
-
-    return this.httpClient.get<PagingResponse<ListingTableRow>>(endpointUrl);
-  }
-
-  getAggregatedListings(
-    pageNumber: number = 1,
-    pageSize: number = 10,
-    orderBy: string = '',
-    direction: 'asc' | 'desc' = 'asc',
-    searchReq: ListingSearchRequest = {},
-    filter?: ListingFilter
-  ): Observable<PagingResponse<AggregatedListingTableRow>> {
-    let endpointUrl = `${environment.API_HOST}/rentallistings/grouped?pageSize=${pageSize}&pageNumber=${pageNumber}`;
-
-    if (orderBy) {
-      endpointUrl += `&orderBy=${orderBy}&direction=${direction}`;
-    }
-
-    if (searchReq.all) {
-      endpointUrl += `&all=${searchReq.all}`;
-    }
-    if (searchReq.address) {
-      endpointUrl += `&address=${searchReq.address}`;
-    }
-    if (searchReq.url) {
-      endpointUrl += `&url=${searchReq.url}`;
-    }
-    if (searchReq.listingId) {
-      endpointUrl += `&listingId=${searchReq.listingId}`;
-    }
-    if (searchReq.hostName) {
-      endpointUrl += `&hostName=${searchReq.hostName}`;
-    }
-    if (searchReq.businessLicence) {
-      endpointUrl += `&businessLicence=${searchReq.businessLicence}`;
-    }
-
-    if (filter) {
-      if (filter.byLocation) {
-        if (!!filter.byLocation?.isPrincipalResidenceRequired) {
-          endpointUrl += `&prRequirement=${filter.byLocation.isPrincipalResidenceRequired == 'Yes'}`;
-        }
-        if (!!filter.byLocation?.isBusinessLicenceRequired) {
-          endpointUrl += `&blRequirement=${filter.byLocation.isBusinessLicenceRequired == 'Yes'}`;
-        }
-      }
-      if (filter.byStatus) {
-        if (filter.byStatus.reassigned !== null && filter.byStatus.reassigned !== undefined) {
-          endpointUrl += `&reassigned=${!!filter.byStatus.reassigned}`;
-        }
-        if (filter.byStatus.takedownComplete !== null && filter.byStatus.takedownComplete !== undefined) {
-          endpointUrl += `&takedownComplete=${!!filter.byStatus.takedownComplete}`;
+        if (orderBy) {
+            url += `&orderBy=${orderBy}&direction=${direction}`;
         }
 
-        const statuses = new Array();
-        if (filter.byStatus.active) statuses.push('A')
-        if (filter.byStatus.inactive) statuses.push('I')
-        if (filter.byStatus.new) statuses.push('N')
-
-        if (statuses.length) {
-          endpointUrl += `&statuses=${statuses.join(',')}`;
-        }
-      }
-      if (!!filter.community) {
-        endpointUrl += `&lgId=${filter.community}`;
-      }
+        return this.httpClient.get<PagingResponse<ListingUploadHistoryRecord>>(url);
     }
 
-    return this.httpClient.get<PagingResponse<AggregatedListingTableRow>>(endpointUrl);
-  }
+    getUploadHistoryErrors(id: number): Observable<any> {
+        return this.httpClient.get<any>(
+            `${environment.API_HOST}/rentallistingreports/uploads/${id}/errorfile`,
+            { headers: this.textHeaders, responseType: 'text' as 'json' },
+        );
+    }
 
-  getListingDetailsById(id: number): Observable<ListingDetails> {
-    return this.httpClient.get<ListingDetails>(`${environment.API_HOST}/rentallistings/${id}`);
-  }
+    getListings(
+        pageNumber: number = 1,
+        pageSize: number = 10,
+        orderBy: string = '',
+        direction: 'asc' | 'desc' = 'asc',
+        searchReq: ListingSearchRequest = {},
+        filter?: ListingFilter,
+    ): Observable<PagingResponse<ListingTableRow>> {
+        let endpointUrl = `${environment.API_HOST}/rentallistings?pageSize=${pageSize}&pageNumber=${pageNumber}`;
 
-  getJurisdictions(): Observable<Array<ExportJurisdiction>> {
-    return this.httpClient.get<Array<ExportJurisdiction>>(`${environment.API_HOST}/rentallistings/exports`);
-  }
+        if (orderBy) {
+            endpointUrl += `&orderBy=${orderBy}&direction=${direction}`;
+        }
 
-  downloadListings(jurisdictionId: number): Observable<any> {
-    return this.httpClient.get(`${environment.API_HOST}/rentallistings/exports/${jurisdictionId}`,
-      { responseType: 'arraybuffer' });
-  }
+        if (searchReq.all) {
+            endpointUrl += `&all=${searchReq.all}`;
+        }
+        if (searchReq.address) {
+            endpointUrl += `&address=${searchReq.address}`;
+        }
+        if (searchReq.url) {
+            endpointUrl += `&url=${searchReq.url}`;
+        }
+        if (searchReq.listingId) {
+            endpointUrl += `&listingId=${searchReq.listingId}`;
+        }
+        if (searchReq.hostName) {
+            endpointUrl += `&hostName=${searchReq.hostName}`;
+        }
+        if (searchReq.businessLicence) {
+            endpointUrl += `&businessLicence=${searchReq.businessLicence}`;
+        }
 
-  getAddressCandidates(addressString: string): Observable<Array<ListingAddressCandidate>> {
-    return this.httpClient.get<Array<ListingAddressCandidate>>(`${environment.API_HOST}/rentallistings/addresses/candidates`, {
-      params: {
-        addressString,
-      }
-    });
-  }
+        if (filter) {
+            if (filter.byLocation) {
+                if (!!filter.byLocation?.isPrincipalResidenceRequired) {
+                    endpointUrl += `&prRequirement=${
+                        filter.byLocation.isPrincipalResidenceRequired == 'Yes'
+                    }`;
+                }
+                if (!!filter.byLocation?.isBusinessLicenceRequired) {
+                    endpointUrl += `&blRequirement=${
+                        filter.byLocation.isBusinessLicenceRequired == 'Yes'
+                    }`;
+                }
+            }
+            if (filter.byStatus) {
+                if (
+                    filter.byStatus.reassigned !== null &&
+                    filter.byStatus.reassigned !== undefined
+                ) {
+                    endpointUrl += `&reassigned=${!!filter.byStatus.reassigned}`;
+                }
+                if (
+                    filter.byStatus.takedownComplete !== null &&
+                    filter.byStatus.takedownComplete !== undefined
+                ) {
+                    endpointUrl += `&takedownComplete=${!!filter.byStatus.takedownComplete}`;
+                }
 
-  changeAddress(id: number, addressString: string): Observable<any> {
-    return this.httpClient.put<any>(`${environment.API_HOST}/rentallistings/${id}/address`, {
-      rentalListingId: id,
-      addressString,
-    });
-  }
+                const statuses = new Array();
+                if (filter.byStatus.active) statuses.push('A');
+                if (filter.byStatus.inactive) statuses.push('I');
+                if (filter.byStatus.new) statuses.push('N');
 
-  confirmAddress(id: number): Observable<any> {
-    return this.httpClient.put<any>(`${environment.API_HOST}/rentallistings/${id}/address/confirm`, {});
-  }
+                if (statuses.length) {
+                    endpointUrl += `&statuses=${statuses.join(',')}`;
+                }
+            }
+            if (!!filter.community) {
+                endpointUrl += `&lgId=${filter.community}`;
+            }
+        }
+
+        return this.httpClient.get<PagingResponse<ListingTableRow>>(endpointUrl);
+    }
+
+    getAggregatedListings(
+        pageNumber: number = 1,
+        pageSize: number = 10,
+        orderBy: string = '',
+        direction: 'asc' | 'desc' = 'asc',
+        searchReq: ListingSearchRequest = {},
+        filter?: ListingFilter,
+    ): Observable<{ count: number; listings: PagingResponse<AggregatedListingTableRow> }> {
+        let listingsEndpointUrl = `${environment.API_HOST}/rentallistings/grouped?pageSize=${pageSize}&pageNumber=${pageNumber}`;
+
+        if (orderBy) {
+            listingsEndpointUrl += `&orderBy=${orderBy}&direction=${direction}`;
+        }
+
+        if (searchReq.all) {
+            listingsEndpointUrl += `&all=${searchReq.all}`;
+        }
+        if (searchReq.address) {
+            listingsEndpointUrl += `&address=${searchReq.address}`;
+        }
+        if (searchReq.url) {
+            listingsEndpointUrl += `&url=${searchReq.url}`;
+        }
+        if (searchReq.listingId) {
+            listingsEndpointUrl += `&listingId=${searchReq.listingId}`;
+        }
+        if (searchReq.hostName) {
+            listingsEndpointUrl += `&hostName=${searchReq.hostName}`;
+        }
+        if (searchReq.businessLicence) {
+            listingsEndpointUrl += `&businessLicence=${searchReq.businessLicence}`;
+        }
+
+        if (filter) {
+            if (filter.byLocation) {
+                if (!!filter.byLocation?.isPrincipalResidenceRequired) {
+                    listingsEndpointUrl += `&prRequirement=${
+                        filter.byLocation.isPrincipalResidenceRequired == 'Yes'
+                    }`;
+                }
+                if (!!filter.byLocation?.isBusinessLicenceRequired) {
+                    listingsEndpointUrl += `&blRequirement=${
+                        filter.byLocation.isBusinessLicenceRequired == 'Yes'
+                    }`;
+                }
+            }
+            if (filter.byStatus) {
+                if (
+                    filter.byStatus.reassigned !== null &&
+                    filter.byStatus.reassigned !== undefined
+                ) {
+                    listingsEndpointUrl += `&reassigned=${!!filter.byStatus.reassigned}`;
+                }
+                if (
+                    filter.byStatus.takedownComplete !== null &&
+                    filter.byStatus.takedownComplete !== undefined
+                ) {
+                    listingsEndpointUrl += `&takedownComplete=${!!filter.byStatus
+                        .takedownComplete}`;
+                }
+
+                const statuses = new Array();
+                if (filter.byStatus.active) statuses.push('A');
+                if (filter.byStatus.inactive) statuses.push('I');
+                if (filter.byStatus.new) statuses.push('N');
+
+                if (statuses.length) {
+                    listingsEndpointUrl += `&statuses=${statuses.join(',')}`;
+                }
+            }
+            if (!!filter.community) {
+                listingsEndpointUrl += `&lgId=${filter.community}`;
+            }
+        }
+
+        const listingsCountEndpointUrl = `${
+            environment.API_HOST
+        }/rentallistings/grouped/count${listingsEndpointUrl.substring(
+            listingsEndpointUrl.indexOf('?'),
+        )}`;
+
+        //return this.httpClient.get<PagingResponse<AggregatedListingTableRow>>(listingsEndpointUrl);
+        return forkJoin({
+            count: this.httpClient.get<number>(listingsCountEndpointUrl), // First request: Get the total count
+            listings:
+                this.httpClient.get<PagingResponse<AggregatedListingTableRow>>(listingsEndpointUrl), // Second request: Get the listings
+        });
+    }
+
+    getListingDetailsById(id: number): Observable<ListingDetails> {
+        return this.httpClient.get<ListingDetails>(`${environment.API_HOST}/rentallistings/${id}`);
+    }
+
+    getJurisdictions(): Observable<Array<ExportJurisdiction>> {
+        return this.httpClient.get<Array<ExportJurisdiction>>(
+            `${environment.API_HOST}/rentallistings/exports`,
+        );
+    }
+
+    downloadListings(jurisdictionId: number): Observable<any> {
+        return this.httpClient.get(
+            `${environment.API_HOST}/rentallistings/exports/${jurisdictionId}`,
+            { responseType: 'arraybuffer' },
+        );
+    }
+
+    getAddressCandidates(addressString: string): Observable<Array<ListingAddressCandidate>> {
+        return this.httpClient.get<Array<ListingAddressCandidate>>(
+            `${environment.API_HOST}/rentallistings/addresses/candidates`,
+            {
+                params: {
+                    addressString,
+                },
+            },
+        );
+    }
+
+    changeAddress(id: number, addressString: string): Observable<any> {
+        return this.httpClient.put<any>(`${environment.API_HOST}/rentallistings/${id}/address`, {
+            rentalListingId: id,
+            addressString,
+        });
+    }
+
+    confirmAddress(id: number): Observable<any> {
+        return this.httpClient.put<any>(
+            `${environment.API_HOST}/rentallistings/${id}/address/confirm`,
+            {},
+        );
+    }
 }

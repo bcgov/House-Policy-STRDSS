d114ba52dd7fd54cfdb4e49d993c906d6bdb431d
ychung-mot <young-jin.chung@dxcas.com>
2024-09-26 14:42:46 -0700

    feat(dss-364)

--- server/StrDss.Model/OrganizationDtos/PlatformViewDto.cs
+++ server/StrDss.Model/OrganizationDtos/PlatformViewDto.cs
@@ -8,14 +8,19 @@
         }
 
         public long OrganizationId { get; set; }
+        public string OrganizationType { get; set; } = null!;
         public string OrganizationCd { get; set; } = null!;
         public string OrganizationNm { get; set; } = null!;
         public DateTime UpdDtm { get; set; }
         public Guid? UpdUserGuid { get; set; }
-        public long NoticeOfTakedownContactId { get; set; }
-        public string NoticeOfTakedownContactEmail { get; set; }
-        public long TakedownRequestContactId { get; set; }
-        public string TakedownRequestContactEmail { get; set; }
+        public long? NoticeOfTakedownContactId1 { get; set; }
+        public string? NoticeOfTakedownContactEmail1 { get; set; }
+        public long? TakedownRequestContactId1 { get; set; }
+        public string? TakedownRequestContactEmail1 { get; set; }
+        public long? NoticeOfTakedownContactId2 { get; set; }
+        public string? NoticeOfTakedownContactEmail2 { get; set; }
+        public long? TakedownRequestContactId2 { get; set; }
+        public string? TakedownRequestContactEmail2 { get; set; }
         public virtual ICollection<PlatformViewDto> Subsidiaries { get; set; }
     }
 }

7e3603b4b327e49bdf2f0c7c3adc25e10158ee1c
ychung-mot <young-jin.chung@dxcas.com>
2024-10-15 09:30:58 -0700

    chore: scaffold

--- server/StrDss.Data/Entities/DssOrganizationContactPerson.cs
+++ server/StrDss.Data/Entities/DssOrganizationContactPerson.cs
@@ -14,11 +14,6 @@ public partial class DssOrganizationContactPerson
     public long OrganizationContactPersonId { get; set; }
 
     /// <summary>
-    /// E-mail address given for the contact by the organization
-    /// </summary>
-    public string EmailAddressDsc { get; set; } = null!;
-
-    /// <summary>
     /// Indicates whether the contact should receive all communications directed at the organization
     /// </summary>
     public bool? IsPrimary { get; set; }
@@ -39,14 +34,14 @@ public partial class DssOrganizationContactPerson
     public string? PhoneNo { get; set; }
 
     /// <summary>
-    /// Foreign key
+    /// E-mail address given for the contact by the organization
     /// </summary>
-    public long ContactedThroughOrganizationId { get; set; }
+    public string EmailAddressDsc { get; set; } = null!;
 
     /// <summary>
     /// Foreign key
     /// </summary>
-    public string? EmailMessageType { get; set; }
+    public long ContactedThroughOrganizationId { get; set; }
 
     /// <summary>
     /// Trigger-updated timestamp of last change
@@ -57,6 +52,11 @@ public partial class DssOrganizationContactPerson
     /// The globally unique identifier (assigned by the identity provider) for the most recent user to record a change
     /// </summary>
     public Guid? UpdUserGuid { get; set; }
+
+    /// <summary>
+    /// Foreign key
+    /// </summary>
+    public string? EmailMessageType { get; set; }
 
     public virtual DssOrganization ContactedThroughOrganization { get; set; } = null!;
 

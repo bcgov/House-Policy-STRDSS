a654c93f412fe274726807436642cfc5cf7bafe1
Rick Anderson <Richand101@hotmail.com>
2024-10-16 13:03:01 -0700

    @DSS-883 Created API endpoint /api/organizations/platformTypeDropdown

---
+++ server/StrDss.Model/OrganizationDtos/PlatformTypeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StrDss.Model.OrganizationDtos
+{
+    public class PlatformTypeDto
+    {
+        [JsonPropertyName("value")]
+        public string PlatformType { get; set; }
+        [JsonPropertyName("label")]
+        public string PlatformTypeNm { get; set; }
+    }
+}

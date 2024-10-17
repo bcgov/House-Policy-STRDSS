490b7af1e0c954d323e37b922c3d1db209cd9569
ychung-mot <young-jin.chung@dxcas.com>
2024-10-17 10:43:26 -0700

    chore: fix compile errors

--- server/StrDss.Model/OrganizationDtos/PlatformSubUpdateDto.cs
+++ server/StrDss.Model/OrganizationDtos/PlatformSubUpdateDto.cs
@@ -4,7 +4,6 @@
     {
         public string OrganizationCd { get; }
         public long OrganizationId { get; set; }
-        public string OrganizationCd { get; set; } = null;
         public string OrganizationNm { get; set; } = null!;
         public long ManagingOrganizationId { get; set; }
         public bool? IsActive { get; set; }

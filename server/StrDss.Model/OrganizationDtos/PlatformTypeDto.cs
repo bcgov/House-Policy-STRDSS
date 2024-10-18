8c9c4c45ea2115d634d715b6e98a35232467614b
Rick Anderson <Richand101@hotmail.com>
2024-10-18 12:27:15 -0700

    Swapped Label and Value for PlatformTypeDto for FE

--- server/StrDss.Model/OrganizationDtos/PlatformTypeDto.cs
+++ server/StrDss.Model/OrganizationDtos/PlatformTypeDto.cs
@@ -5,8 +5,11 @@ namespace StrDss.Model.OrganizationDtos
     public class PlatformTypeDto
     {
         [JsonPropertyName("label")]
-        public string PlatformType { get; set; }
-        [JsonPropertyName("value")]
         public string PlatformTypeNm { get; set; }
-    }
+
+        [JsonPropertyName("value")]
+        public string PlatformType { get; set; }
+    } 
+
+        
 }

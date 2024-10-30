5dc0d5a68d8bfa81ebed0fed3957bec2601b3bcb
Rick Anderson <Richand101@hotmail.com>
2024-10-30 14:13:12 -0700

    Completed AddSubsidiaryPlatform test

---
+++ Test/UITest/TestFrameWork/Models/ManagePlatformsPageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UITest.Models
+{
+    public class ManagePlatformsPageModel
+    {
+
+        public static string EditPlatformButton { get => "#platform-edit-0-icon"; }
+                                                       
+        public static string ListingsTable { get => "listings-table"; }
+
+    }
+}

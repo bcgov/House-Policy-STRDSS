0241bf4d79b937cf0b817510b379d82598449cbf
ychung-mot <young-jin.chung@dxcas.com>
2024-10-31 10:50:23 -0700

    chore: nullable bl format

--- server/StrDss.Model/OrganizationDtos/LocalGovUpdateDto.cs
+++ server/StrDss.Model/OrganizationDtos/LocalGovUpdateDto.cs
@@ -5,7 +5,7 @@
         public long OrganizationId { get; set; }
         public string OrganizationNm { get; set; } = "";
         public string LocalGovernmentType { get; set; } = "";
-        public string BusinessLicenceFormatTxt { get; set; } = "";
+        public string? BusinessLicenceFormatTxt { get; set; } = "";
         public DateTime UpdDtm { get; set; }
     }
 }

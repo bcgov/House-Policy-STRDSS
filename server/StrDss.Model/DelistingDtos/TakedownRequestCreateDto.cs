7db101f44d08464550cf3004085af87813d56808
ychung-mot <young-jin.chung@dxcas.com>
2024-11-26 10:44:31 -0800

    chore: added back CustomDetailTxt to TD request

--- server/StrDss.Model/DelistingDtos/TakedownRequestCreateDto.cs
+++ server/StrDss.Model/DelistingDtos/TakedownRequestCreateDto.cs
@@ -9,6 +9,7 @@ namespace StrDss.Model.DelistingDtos
         public string ListingUrl { get; set; } = "";
         public List<string> CcList { get; set; } = new List<string>();
         public bool IsWithStandardDetail { get; set; }
+        public string CustomDetailTxt { get; set; } = "";
         [JsonIgnore]
         public List<string> ToList { get; set; } = new List<string>();
     }

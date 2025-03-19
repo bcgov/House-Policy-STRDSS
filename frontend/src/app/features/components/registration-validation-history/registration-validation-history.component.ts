d6c41b005cc7727aa0a19c54695b4a0bc944d044
Sai Krishna Metpalli <310911@NTTDATA.COM>
2025-03-19 09:47:52 -0700

    DSS-1115-1116 Added permission, menu and components

---
+++ frontend/src/app/features/components/registration-validation-history/registration-validation-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-validation-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-validation-history.component.html',
+  styleUrl: './registration-validation-history.component.scss'
+})
+export class RegistrationValidationHistoryComponent {
+
+}
---
+++ frontend/src/app/features/components/validate-registration-data/validate-registration-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-registration-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './validate-registration-data.component.html',
+  styleUrl: './validate-registration-data.component.scss'
+})
+export class ValidateRegistrationDataComponent {
+
+}

07cb8a6953642b61f3e3487e63a4c3177c47a277
ChrisHoban <49213216+ChrisHoban@users.noreply.github.com>
2026-07-20 15:18:59 -0700

    Increased Listing ID and STR Unit to 50 char limit in validation

--- server/StrDss.Service/RentalListingReportValidationRule.cs
+++ server/StrDss.Service/RentalListingReportValidationRule.cs
@@ -30,7 +30,7 @@ namespace StrDss.Service
                 FieldName = RentalListingReportFields.ListingId,
                 FieldType = FieldTypes.String,
                 Required = true,
-                MaxLength = 25,
+                MaxLength = 50,
             });
 
             rules.Add(new FieldValidationRule
@@ -76,7 +76,7 @@ namespace StrDss.Service
                 FieldName = RentalListingReportFields.RentalUnit,
                 FieldType = FieldTypes.String,
                 Required = false,
-                MaxLength = 100
+                MaxLength = 50
             });
 
             rules.Add(new FieldValidationRule
